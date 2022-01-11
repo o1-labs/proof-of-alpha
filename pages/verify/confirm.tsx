c205f5b41f67bfbf6fb0a0d7ecacdf760df2b4a1
ymekuria <ymekuria@gmail.com>
2022-01-10 20:54:01 -0700

    feat: add zero knowledge proof confirm page

---
+++ pages/verify/confirm.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const Confirm: NextPage = () => {
+  return (
+    <div className="flex min-h-screen bg-background bg-cover bg-no-repeat">
+      Confirm
+    </div>
+  );
+};
+
+export default Confirm;
