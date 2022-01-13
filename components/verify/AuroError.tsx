72b4c77a10de351d022ff2f4c3cd699b5b40eae9
ymekuria <ymekuria@gmail.com>
2022-01-12 22:39:14 -0700

    feat: add no installed auro wallet error component content and styles

--- components/verify/AuroError.tsx
+++ components/verify/AuroError.tsx
@@ -1,7 +1,32 @@
-import type { NextPage } from 'next';
+import Image from 'next/image';
+import ExclamationIcon from '../../public/assets/verify/finalize/exclamation-icon.png';
+import ExternalLinkIcon from '../../public/assets/common/external-link-icon.png';
 
 const AuroError = () => {
-  return <div>AuroError</div>;
+  return (
+    <>
+      <div className="flex items-center justify-end  ">
+        <Image src={ExclamationIcon} alt="info" />
+        <span className="text-2xl leading-loose ml-2 ">
+          Auro Wallet must be installed in your browser to continue.
+        </span>
+      </div>
+      <div className="flex items-center justify-end  ">
+        <a
+          className="flex"
+          target="_blank"
+          href={
+            'https://chrome.google.com/webstore/detail/auro-walletmina-protocol/cnmamaachppnkjgnildpdmkaakejnhae'
+          }
+        >
+          <Image src={ExternalLinkIcon} alt="info" />
+          <span className="text-2x underline leading-loose ml-2 ">
+            GET AURO
+          </span>
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default AuroError;
