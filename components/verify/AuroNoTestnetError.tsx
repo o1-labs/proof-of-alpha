d200697a8b8b9d1bd94f16235d9e3dcb0f9c6db9
ymekuria <ymekuria@gmail.com>
2022-01-13 17:30:10 -0700

    feat: add content and styles to auro wallet not connected to testnet error component

--- components/verify/AuroNoTestnetError.tsx
+++ components/verify/AuroNoTestnetError.tsx
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+import ExclamationIcon from '../../public/assets/verify/finalize/exclamation-icon.png';
+
 const AuroNoTestnetError = () => {
-  return <div>AuroNoTestnetError</div>;
+  return (
+    <>
+      <div className="flex items-center justify-end  ">
+        <Image src={ExclamationIcon} alt="info" />
+        <span className="text-2xl leading-loose ml-2 ">
+          Auro Wallet must be connected to Snapps Testnet to continue.
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default AuroNoTestnetError;
