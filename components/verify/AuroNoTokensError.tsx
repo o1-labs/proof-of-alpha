1057f05ba387c9fb8a23fcf2291c6c860473763d
ymekuria <ymekuria@gmail.com>
2022-01-13 23:20:55 -0700

    feat: add no auro tokens error component content and styles

--- components/verify/AuroNoTokensError.tsx
+++ components/verify/AuroNoTokensError.tsx
@@ -1,5 +1,31 @@
+import Image from 'next/image';
+import ExclamationIcon from '../../public/assets/verify/finalize/exclamation-icon.png';
+import ExternalLinkIcon from '../../public/assets/common/external-link-icon.png';
+
 const AuroNoTokensError = () => {
-  return <div>AuroNoTokensError</div>;
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Image src={ExclamationIcon} alt="info" />
+        <span className="text-2xl leading-loose ml-2 ">
+          Auro Wallet must have testnet tokens to continue.
+        </span>
+      </div>
+      <div className="flex justify-end">
+        <a
+          className="flex"
+          target="_blank"
+          href={'https://faucet.minaprotocol.com'}
+        >
+          <Image src={ExternalLinkIcon} alt="info" />
+
+          <span className="flex items-center text-2x underline leading-loose ml-2 ">
+            MINA FAUCET
+          </span>
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default AuroNoTokensError;
