1323eb90b9f82a9a233cd728b2173de03708aae0
ymekuria <ymekuria@gmail.com>
2022-01-19 02:21:38 -0700

    feat: add moblie responsive styles to home proof section

--- components/home/ProofSection.tsx
+++ components/home/ProofSection.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
-
 import Button from '../common/Button';
-import TransparentButton from '../common/TransparentButton';
-import Checkmark from '../../public/assets/common/checkmark.png';
-import ZKBorderLogo from '../../public/assets/home/zk-border-logo.png';
-
 import CheckmarkItem from './CheckmarkItem';
+import TransparentButton from '../common/TransparentButton';
+import ZKBorderLogo from '../../public/assets/home/zk-logo-home.png';
 
 const ProofSection = () => {
   return (
@@ -14,9 +11,9 @@ const ProofSection = () => {
       className="flex min-h-screen bg-blue-background bg-cover bg-no-repeat "
       id="proof"
     >
-      <div className="flex flex-col items-center justify-center h-screen mt-48 ml-64">
-        <div className="container h-screen flex flex-col  space-y-8 w-124  text-white ">
-          <p className="text-6xl leading-snug font-normal mb-10 ">
+      <div className="flex flex-col md:flex-row items-center justify-center h-screen mt-24 md:mt-48 md:ml-64">
+        <div className="container h-screen flex flex-col space-y-5 w-4/5 md:space-y-8  md:w-124  text-white ">
+          <p className="text-xl w-[290px] md:w-auto text-right md:text-left md:text-6xl leading-snug font-normal mb-4 md:mb-10 ">
             Your Proof of Trade Statement:
           </p>
           <CheckmarkItem copy="Verifies your trades, over any time peroid" />
@@ -25,26 +22,26 @@ const ProofSection = () => {
           <CheckmarkItem copy="Keeps you in control of what you share" />
           <CheckmarkItem copy="100% open source & on-chain" />
 
-          <div className="">
-            <div className="flex flex-row items-center h-auto justify-between  mt-30">
+          <div className="ml-8 md:ml-0">
+            <div className="flex flex-col  md:flex-row items-center justify-center h-auto md:justify-between  md:w-2/3 mt-12 md:mt-30">
               <Link href={'/start'} passHref>
                 <Button
-                  buttonStyle="px-12  md:w-auto  text-xl md:text-2xl"
+                  buttonStyle="px-7 text-sm md:text-2xl"
                   label="LAUNCH APP"
                 />
               </Link>
 
               <Link href={'#exchange'}>
                 <TransparentButton
-                  buttonStyle="-ml-3 text-2xl"
+                  buttonStyle="md:-ml-3 text-sm md:text-2xl"
                   label="LEARN MORE"
                 />
               </Link>
             </div>
           </div>
         </div>
-        {/* container */}
-        <div className="w-86 h-86 mt-44 ml-64 relative">
+
+        <div className="hidden md:block mw-86 h-86  mb-100 ml-24 relative">
           <Image src={ZKBorderLogo} alt="ZK logo" />
         </div>
       </div>
