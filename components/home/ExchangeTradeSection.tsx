cdc3f4159c98d765b884984e9efbb37b88be1843
ymekuria <ymekuria@gmail.com>
2022-01-20 03:17:37 -0700

    feat: add mobile responsive choice home page section

--- components/home/ExchangeTradeSection.tsx
+++ components/home/ExchangeTradeSection.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Button from '../common/Button';
 import Indicator from './Indicator';
-import TwitterCardLeft from '../../public/assets/common/twitter-card.png';
+import TwitterCard from '../../public/assets/common/twitter-card.png';
 import RightArrow from '../../public/assets/common/right-arrow.png';
 
 const ExchangeTradeSection = () => {
@@ -11,9 +11,9 @@ const ExchangeTradeSection = () => {
       className="flex justify-center md:justify-start min-h-screen min-w-screen bg-red-background bg-cover bg-no-repeat "
       id="exchange"
     >
-      <div className="flex w-[285px] justify-center md:space-x-44 md:w-auto h-screen md:mt-56 ">
+      <div className="flex w-[290px] justify-center md:space-x-44 md:w-auto h-screen md:mt-56 ">
         <div className="hidden  md:block w-[1013px] -ml-[560px] mt-10 relative">
-          <Image src={TwitterCardLeft} alt="Twitter Card" />
+          <Image src={TwitterCard} alt="Twitter Card" />
         </div>
         {/* container */}
         <div className="flex justify-center md:items-center h-screen w-screen mt-16 md:mt-0 md:w-[690px] md:space-y-5 ">
@@ -40,7 +40,7 @@ const ExchangeTradeSection = () => {
             {/* end copy section */}
 
             {/* indicator button group  */}
-            <div className="flex flex-col md:flex-row items-center justify-center md:justify-between space-y-9 md:space-y-0 mt-10 md:mt-32">
+            <div className="flex flex-col md:flex-row items-center justify-center md:justify-between space-y-9 md:space-y-0 mt-20 md:mt-32">
               <Link href={'/start'} passHref>
                 <Button
                   buttonStyle="px-20 w-4/5 md:w-1/3 text-base md:text-2xl "
@@ -52,7 +52,7 @@ const ExchangeTradeSection = () => {
             {/* end indicator button group */}
           </div>
         </div>
-        <div className="w-18 md:w-40 md:h-40 mt-64 md:mt-52 -mr-12 md:mr-0   relative">
+        <div className="w-20 md:w-40 md:h-40 mt-64 md:mt-52 -mr-12 md:mr-0   relative">
           <Link href={'/learn'}>
             <Image src={RightArrow} alt="Right Arrow" />
           </Link>
--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -2,56 +2,51 @@ import Link from 'next/link';
 import Image from 'next/image';
 import type { NextPage } from 'next';
 import Button from '../../components/common/Button';
+import Indicator from '../../components/home/Indicator';
 import RightArrow from '../../public/assets/common/right-arrow.png';
 import LeftArrow from '../../public/assets/common/left-arrow.png';
 
 const Learn: NextPage = () => {
   return (
     <div
-      className="flex min-h-screen bg-red-background bg-cover bg-no-repeat "
+      className="flex min-h-screen bg-red-background bg-cover bg-no-repeat text-white"
       id="exchange"
     >
-      <div className="flex h-2/3 mt-64 ml-44 items-center space-x-24 ">
-        <div className="w-52 h-52  relative">
+      <div className="flex  items-center  mt-[88px] h-4/5 md:w-auto md:h-2/3 md:mt-64 md:ml-44  md:space-x-24 ">
+        <div className="flex items-center  w-14 md:w-40 md:h-40  mb-12 md:mb-0 relative">
           <Link href={'/#exchange'}>
             <Image src={LeftArrow} alt="Right Arrow" />
           </Link>
         </div>
-        <div className="flex-col w-115 space-y-11">
-          <p className="text-6xl leading-snug  text-white">
-            The Choice is Yours
-          </p>
+        <div className="flex-col w-[275px] md:w-115 md:space-y-11">
+          <div className="md:mb-32">
+            <p className="text-xl md:text-6xl text-center md:text-left mb-8">
+              The Choice is Yours
+            </p>
 
-          <p className="text-4xl leading-11  text-white">
-            Certify your entire account history, or a recent time period.
-          </p>
-          <p className="text-4xl leading-11  text-white">
-            Focus only on your sick gains, or share fully transparent
-            transaction data.
-          </p>
-          <p className="text-4xl leading-11  text-white">It's up to you.</p>
-
-          <div className="flex items-center ">
-            <div className="w-1/2">
-              <Link href={'/start'} passHref>
-                <Button buttonStyle="px-20" label="NEXT" />
-              </Link>
-            </div>
-            <div className="flex w-1/2">
-              <div className="mx-6">
-                <div className=" bg-custom-gray-indicator-dot w-4 h-4 rounded-full"></div>
-              </div>
-              <div className="mx-6">
-                <div className=" bg-white w-4 h-4 rounded-full"></div>
-              </div>
-              <div className="mx-6">
-                <div className=" bg-custom-gray-indicator-dot w-4 h-4 rounded-full"></div>
-              </div>
-            </div>
+            <p className="text-base md:text-4xl leading-loose md:leading-11 mb-5 ">
+              Certify your entire account history, or a recent time period.
+            </p>
+            <p className="text-base md:text-4xl leading-loose md:leading-11">
+              Focus only on your sick gains, or share fully transparent
+              transaction data.
+            </p>
+            <p className="text-base md:text-4xl leading-loose md:leading-11 my-4 ">
+              It's up to you.
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row items-center justify-center md:justify-between space-y-9 md:space-y-0 mt-24 md:mt-40">
+            <Link href={'/start'} passHref>
+              <Button
+                buttonStyle="px-20 w-4/5 md:w-1/3 text-base md:text-2xl "
+                label="NEXT"
+              />
+            </Link>
+            <Indicator page="second" />
           </div>
         </div>
-        <div className="w-52 h-52  relative">
-          <Link href={'/learn/data'}>
+        <div className="flex items-center w-14 md:w-40 md:h-40 mb-12 md:mb-0 relative">
+          <Link href={'/learn/data'} passHref>
             <Image src={RightArrow} alt="Right Arrow" />
           </Link>
         </div>
