1467b4ae86dff61963306e654dde00d221a0bac6
ymekuria <ymekuria@gmail.com>
2022-01-21 04:07:39 -0700

    fix: update responsive styles

--- components/start/Header.tsx
+++ components/start/Header.tsx
@@ -3,12 +3,21 @@ import ZKLogo from '../../public/assets/common/zk-logo.png';
 
 const Header = () => {
   return (
+    // <header className="">
+    //   <div className="ml-30 pt-20 h-auto w-32 relative">
+    //     <Image src={ZKLogo} alt="zk logo" />
+    //   </div>
+    // </header>
     <header className="">
-      <div className="ml-30 pt-20 h-auto w-32 relative">
+      <div className="ml-36 mt-5 items-center w-20 md:w-24 md:ml-18  xl:ml-28 md:mt-5 xl:mt-12  xl:h-32 xl:w-auto absolute">
         <Image src={ZKLogo} alt="zk logo" />
       </div>
     </header>
   );
 };
-
+<header className="">
+  <div className="ml-36 mt-5 items-center w-20 md:w-24 md:ml-7  xl:ml-10 md:mt-5 xl:mt-12  xl:h-32 xl:w-auto absolute">
+    <Image src={ZKLogo} alt="zk logo" />
+  </div>
+</header>;
 export default Header;
--- pages/start/index.tsx
+++ pages/start/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import type { NextPage } from 'next';
-import tradeTwitter from '../../public/assets/start/trade-history-with-twitter.png';
+import TradeTwitter from '../../public/assets/start/trade-history-with-twitter.png';
 import Button from '../../components/common/Button';
 import Header from '../../components/start/Header';
 
@@ -10,33 +10,36 @@ const Start: NextPage = () => {
     <div className="min-h-screen bg-background bg-cover bg-no-repeat">
       <Header />
       <div className="flex">
-        <div className="flex flex-col  space-y-11 text-white ml-30 mt-10 w-122">
-          <p className="text-5xl leading-snug  font-medium">
-            Your Free Proof of Trade Statement and Share Badge
+        <div className="flex flex-col md:space-y-4 xl:space-y-9  text-white xl:ml-36 md:ml-28 md:mt-36 xl:mt-56 md:w-[410px] xl:w-[620px]">
+          <p className="md:text-2xl xl:text-5xl   font-bold pr-8">
+            Your Free Proof of Alpha Statement and Share Badge
           </p>
 
-          <p className="text-4xl font-light leading-snug">
-            Proof of Trade Performance offers third party verification of your
-            trade history.
+          <p className="md:text-xl xl:text-3xl font-light ">
+            Proof of Alpha offers third party verification of your trade
+            history.
           </p>
-          <p className="text-4xl  font-light leading-snug">
+          <p className="md:text-xl xl:text-3xl  font-light ">
             You control what you share, and your personal data stays private.
           </p>
-          <p className="text-4xl  font-light leading-snug">
+          <p className="md:text-xl xl:text-3xl font-light ">
             Your track record, certified on-chain, free of charge.
           </p>
-          <p className="text-4xl  font-light leading-snug">
+          <p className="md:text-xl xl:text-3xl  font-light ">
             Available for Binance. Other exchanges coming soon!
           </p>
-          <div className="">
+          <div className="flex mt-9">
             <Link href="/auro" passHref>
-              <Button buttonStyle="px-20" label="CONTINUE" />
+              <Button
+                buttonStyle="px-20 w-1/2 md:text-base xl:text-2xl"
+                label="CONTINUE"
+              />
             </Link>
           </div>
         </div>
-        <div className=" w-122 h-125 mt-11">
+        <div className="absolute right-0 md:w-[360px] xl:w-[600px] bottom-0 ">
           <Image
-            src={tradeTwitter}
+            src={TradeTwitter}
             alt="trade history and twitter"
             layout="responsive"
           />
