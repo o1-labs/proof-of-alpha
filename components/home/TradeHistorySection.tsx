ed91b7cfc55dd98b81d89d6cf2b6b565c358a3bb
ymekuria <ymekuria@gmail.com>
2022-01-21 00:52:04 -0700

    feat: add mobile trade history home page section

--- components/home/TradeHistorySection.tsx
+++ components/home/TradeHistorySection.tsx
@@ -2,37 +2,38 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Button from '../common/Button';
 import TransparentButton from '../common/TransparentButton';
+import CardStatmentGroup from './CardStatementGroup';
 import ZKLogo from '../../public/assets/common/zk-logo.png';
 import TopStamp from '../../public/assets/home/stamp-top.png';
-import ProofOfAlpha from '../../public/assets/home/proof-of-alpha-home.png';
-import TwitterCard from '../../public/assets/common/twitter-card.png';
 
 const TradeHistorySection = () => {
   return (
     <div className="flex h-screen bg-background bg-cover bg-no-repeat">
       <header className="">
-        <div className="ml-18  mt-18  h-32 w-auto absolute">
+        <div className="ml-36 mt-5 items-center w-20   md:ml-10  md:mt-12  md:h-32 md:w-auto absolute">
           <Image src={ZKLogo} alt="zk logo" />
         </div>
       </header>
       <div className="hidden  md:block  absolute  right-0">
         <Image src={TopStamp} alt="Stamp" />
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-center h-screen mt-24 md:mt-40 md:ml-52 ">
-        <div className="container h-screen flex flex-col space-y-5 md:space-y-8    text-white ">
-          <p className="text-7xl text-white">
+
+      <div className="flex flex-col md:flex-row items-center justify-center h-screen mt-30 md:mt-40 md:ml-52 ">
+        <div className="container w-10/12 md:w-auto h-screen flex flex-col space-y-6 md:space-y-8    text-white ">
+          <p className="text-xl md:text-7xl text-white w-full">
             Your Trade History Sets You Apart
           </p>
-          <p className="text-4xl font-light  text-white">
-            But faking gains on the socials is easy with bogus screenshots.
-          </p>
-          <p className="text-4xl font-light text-white">
-            That’s why we built
-            <span className="font-bold"> Proof-of-Alpha </span>
-            using zero-knowledge proofs.
-          </p>
-
-          <div className="ml-12 md:ml-0 z-10">
+          <div className="flex flex-col px-3 md:px-0 space-y-6 md:space-y-8 ">
+            <p className="text-base md:text-4xl font-light  text-white">
+              But faking gains on the socials is easy with bogus screenshots.
+            </p>
+            <p className="text-base md:text-4xl font-light text-white">
+              That’s why we built
+              <span className="font-bold"> Proof-of-Alpha </span>
+              using zero-knowledge proofs.
+            </p>
+          </div>
+          <div className="md:ml-0 z-10">
             <div className="flex flex-col space-y-4 md:flex-row items-center justify-center md:justify-start h-auto   md:w-2/3 mt-10 md:mt-10">
               <Link href={'/start'} passHref>
                 <Button
@@ -51,13 +52,7 @@ const TradeHistorySection = () => {
           </div>
         </div>
       </div>
-
-      <div className="md:absolute z-3 md:w-[840px] bottom-0 md:max-h-[665px] md:right-0 overflow-hidden">
-        <Image src={ProofOfAlpha} alt="Proof of Alpha Stament" />
-      </div>
-      <div className="md:absolute z-5 md:bottom-0 md:w-130 md:left-32 md:max-h-[420px] md:overflow-hidden">
-        <Image src={TwitterCard} alt="Twitter Card" />
-      </div>
+      <CardStatmentGroup />
     </div>
   );
 };
