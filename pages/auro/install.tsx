07eb4ff7628ae8dbcb0c4cc2d903c77fcf0dd2d9
ymekuria <ymekuria@gmail.com>
2022-01-24 02:22:37 -0700

    fix: update auro install page responsive style

--- pages/auro/install.tsx
+++ pages/auro/install.tsx
@@ -7,36 +7,45 @@ import AuroTestnet from '../../public/assets/auro/install/auro-testnet.png';
 
 const Auro: NextPage = () => {
   return (
-    <div className="min-h-screen bg-background bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-background bg-cover bg-no-repeat text-white">
       <Header />
 
       {/* progress bar */}
-      <div className="container w-130 h-screen ml-80">
-        <div className="flex">
-          <span className="border-t-6  border-white w-14 "></span>
-          <span className="border-t-6  border-custom-gray-header-border w-127"></span>
-        </div>
-        {/* progress bar end */}
-
-        <div className="flex flex-col h-screen pr-14 space-y-10 text-white">
-          <p className=" text-1.5xl   tracking-wider leading-9 mt-2">
-            CHOOSE A NETWORK
-          </p>
-          <p className="text-4xl leading-snug ">
-            Make sure you’re connected to Snapps Testnet
-          </p>
-
-          <div className="h-auto">
-            <Image src={AuroTestnet} alt="auro chrome" layout="responsive" />
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col  w-7/12 md:w-6/12 3xl:w-6/12 2xl:w-7/12  container  h-screen">
+          <div className="flex">
+            <span className="border-t-6  border-white w-2/12 "></span>
+            <span className="border-t-6  border-custom-gray-header-border w-10/12"></span>
           </div>
-          <div className="flex justify-between">
-            <Link href={'/auro'} passHref>
-              <TransparentButton label="<< BACK" />
-            </Link>
 
-            <Link href={'/auro/faucet'} passHref>
-              <TransparentButton label="CONTINUE >>" />
-            </Link>
+          {/* progress bar end */}
+
+          <div className="flex flex-col h-screen  space-y-3 ">
+            <p className="text-sm 2xl:text-lg tracking-wider mt-2">
+              CHOOSE A NETWORK
+            </p>
+            <p className="md:text-1.5xl 2xl:text-3.5xl font-medium md:pb-9 2xl:pb-12 2xl:pt-5">
+              Make sure you’re connected to Snapps Testnet
+            </p>
+
+            <div className="h-auto">
+              <Image src={AuroTestnet} alt="auro testnet" layout="responsive" />
+            </div>
+            <div className="flex justify-between pt-8">
+              <Link href={'/auro'} passHref>
+                <TransparentButton
+                  buttonStyle="px-20 md:text-base xl:text-xl"
+                  label="<< BACK"
+                />
+              </Link>
+
+              <Link href={'/auro/faucet'} passHref>
+                <TransparentButton
+                  buttonStyle="px-20 md:text-base xl:text-xl"
+                  label="CONTINUE >>"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
