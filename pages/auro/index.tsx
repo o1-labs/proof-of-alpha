9781acf3c080ca49a6b4137b5401178d7e9d9a68
ymekuria <ymekuria@gmail.com>
2022-01-24 02:21:48 -0700

    fix: updated get auro wallet page style

--- pages/auro/index.tsx
+++ pages/auro/index.tsx
@@ -1,26 +1,21 @@
 import Link from 'next/link';
+
 import Image from 'next/image';
 import type { NextPage } from 'next';
 import Button from '../../components/common/Button';
 import TransparentButton from '../../components/common/TransparentButton';
 import AuroChrome from '../../public/assets/auro/auro-home.png';
 import ZKLogo from '../../public/assets/common/zk-logo.png';
+import Header from '../../components/common/Header';
 
 const Auro: NextPage = () => {
   return (
     <div className="min-h-screen bg-background bg-cover bg-no-repeat text-white">
-      <header className="">
-        {/* container to hold logo and 0x address */}
-        <div className="flex justify-center items-center py-8 relative ">
-          <div className="md:w-24 xl:w-28">
-            <Image src={ZKLogo} alt="zk logo" />
-          </div>
-        </div>
-      </header>
+      <Header />
 
       {/* progress bar */}
       <div className="flex flex-col items-center">
-        <div className="flex flex-col w-7/12 3xl:w-6/12 container  h-screen">
+        <div className="flex flex-col  md:w-6/12 2xl:w-7/12 3xl:w-6/12 container  h-screen">
           <div className="flex">
             {/* <span className="border-t-6  border-white w-14 "></span>
           <span className="border-t-6  border-custom-gray-header-border w-127"></span> */}
@@ -29,14 +24,14 @@ const Auro: NextPage = () => {
           </div>
           {/* progress bar end */}
 
-          <div className="flex flex-col h-screen  space-y-3 ">
-            <p className=" text-lg tracking-wider mt-2">
+          <div className="flex flex-col h-screen md:space-y-2 2xl:space-y-5 ">
+            <p className="text-sm 2xl:text-lg tracking-wider mt-2">
               INSTALL BROWSER WALLET
             </p>
-            <p className="text-3.5xl  font-medium">
+            <p className="md:text-1.5xl 2xl:text-3.5xl  font-medium">
               Install Auro Wallet for Chrome
             </p>
-            <p className="text-1.5xl font-light pb-3">
+            <p className="md:text-base  2xl:text-1.5xl font-light pb-3">
               You'll need Mina's browser wallet to use this app.
             </p>
             <div className="h-auto">
@@ -45,7 +40,7 @@ const Auro: NextPage = () => {
             <div className="flex justify-between pt-8">
               <Link href={'/start'} passHref>
                 <TransparentButton
-                  buttonStyle="px-20 md:text-base xl:text-xl"
+                  buttonStyle="md:text-base 2xl:text-xl"
                   label="<< BACK"
                 />
               </Link>
@@ -58,7 +53,7 @@ const Auro: NextPage = () => {
               >
                 {/* Add open icon */}
                 <Button
-                  buttonStyle="px-20 md:text-base xl:text-xl"
+                  buttonStyle="md:text-base 2xl:text-xl"
                   label="GET AURO"
                 />
               </a>
