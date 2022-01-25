302bb119cdc372660d453e3b37559287917974e3
ymekuria <ymekuria@gmail.com>
2022-01-24 22:50:09 -0700

    chore: rename buttongroup component to daterangegroup

---
+++ components/verify/DateRangeGroup.tsx
@@ -0,0 +1,19 @@
+import LightButton from '../common/LightButton';
+
+const DateRangeGroup = () => {
+  return (
+    <div className="flex items-center">
+      <span className="text-lg 2xl:text-4xl leading-loose  w-1/3">
+        Date range
+      </span>
+      <div className="flex w-2/3">
+        <LightButton buttonStyle="rounded-l w-1/4 " label="LAST 30 DAYS" />
+        <LightButton buttonStyle="w-1/4" label="LAST 90 DAYS" />
+        <LightButton buttonStyle="w-1/4 " label="LAST YEAR" />
+        <LightButton buttonStyle="rounded-r w-1/4 " label="FULL HISTORY" />
+      </div>
+    </div>
+  );
+};
+
+export default DateRangeGroup;
