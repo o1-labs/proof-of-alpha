b41b6c1469eb9dd743b935d3e9bec400ae558a9f
ymekuria <ymekuria@gmail.com>
2022-01-24 18:49:13 -0700

    update: add tooltip for medium size screens

--- components/verify/ToolTip.tsx
+++ components/verify/ToolTip.tsx
@@ -4,15 +4,17 @@ import InfoIcon from '../../public/assets/common/info-icon.png';
 const ToolTip = () => {
   return (
     <div className="relative flex flex-col items-center group">
-      <Image src={InfoIcon} alt="info" />
-      <div className="absolute bottom-0 flex flex-col items-center hidden mb-11 group-hover:flex">
-        <span className=" flex flex-col w-122 relative z-10 p-10 text-2xl text-white leading-snug whitespace-no-wrap bg-black shadow-lg rounded-xl ">
-          <p className="pb-5">
-            <span className="font-bold">Proof of Trade Performance </span>can
-            process up to a maximum of 500 trade fills (total orders fulfilled
-            by the exchange) each time the smart contract is executed.
+      <div className="flex items-center w-4 2xl:w-6">
+        <Image src={InfoIcon} alt="info" />
+      </div>
+      <div className="absolute bottom-0 flex flex-col items-center hidden mb-7 2xl:mb-11 group-hover:flex">
+        <span className=" flex flex-col w-90 2xl:w-122 relative z-10 p-7 2xl:p-10 text-sm 2xl:text-2xl text-white leading-snug whitespace-no-wrap bg-black shadow-lg rounded-xl ">
+          <p className="pb-3 2xl:pb-5">
+            <span className="font-bold">Proof of Alpha </span>can process up to
+            a maximum of 500 trade fills (total orders fulfilled by the
+            exchange) each time the smart contract is executed.
           </p>
-          <p className="pb-5">
+          <p className="pb-3 2xl:pb-5">
             Note that a single order placed by a trader can be broken up by the
             exchange into multiple trade fills.
           </p>
