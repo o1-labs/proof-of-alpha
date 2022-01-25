82daac3f3e793e8aa9d7127824870643a61edac4
ymekuria <ymekuria@gmail.com>
2022-01-24 22:59:53 -0700

    refactor: move token pair group code to a seperate component

---
+++ components/verify/TokenPairGroup.tsx
@@ -0,0 +1,15 @@
+import Dropdown from '../common/Dropdown';
+const TokenPairGroup = () => {
+  return (
+    <div className="flex items-center">
+      <span className="text-lg 2xl:text-4xl leading-loose  w-1/3">
+        Token Pair
+      </span>
+      <div className="flex w-2/3">
+        <Dropdown />
+      </div>
+    </div>
+  );
+};
+
+export default TokenPairGroup;
