24322005805a0caf059ecf65bfb97370b23d6f67
ymekuria <ymekuria@gmail.com>
2022-01-26 19:59:14 -0800

    feat: add renderproofstatus method

--- pages/verify/generate.tsx
+++ pages/verify/generate.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 import LoadingSpinner from '../../components/common/LoadingSpinner';
 import ProofGeneratedConfirmation from '../../components/verify/ProofGeneratedConfirmation';
 import Button from '../../components/common/Button';
 
 const Generate: NextPage = () => {
-  const [isVisable, setIsVisable] = useState<boolean>(false);
+  const [proofConfirmed, setProofConfirmed] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchProofStatus() {
+      // fetch status and setProofConfirmed to true on success => setProofConfirmed(true)
+    }
+    return () => {
+      // cleanup
+    };
+  }, []);
+
+  const renderProofStatus = () => {
+    return proofConfirmed ? (
+      <ProofGeneratedConfirmation />
+    ) : (
+      <LoadingSpinner label="GENERATING ZERO-KNOWLEDGE PROOF" />
+    );
+  };
 
   return (
     <div className="flex min-h-screen bg-background bg-cover bg-no-repeat justify-center">
       <div className="flex container  h-screen justify-center">
         <div className="flex flex-col space-y-6 2xl:space-y-14 text-white mt-10 2xl:mt-20 3xl:mt-36">
-          <div className="flex justify-center">
-            {/* <LoadingSpinner label="GENERATING ZERO-KNOWLEDGE PROOF" /> */}
-            <ProofGeneratedConfirmation />
-          </div>
+          <div className="flex justify-center">{renderProofStatus()}</div>
           <div className="flex text-2xl 2xl:text-3xl 3xl:text-5xl justify-center 2xl:my-11">
             #ZeroKnowledge
           </div>
@@ -47,10 +61,11 @@ const Generate: NextPage = () => {
             </p>
           </div>
           <div className="flex justify-center">
+            {/* Buttton is only visable after a proof is generated */}
             <Button
               label="SEND TO MINA >>"
               buttonStyle="w-1/2 text-xs 2xl:text-xl 3xl:text-2xl"
-              visable={false}
+              visable={proofConfirmed}
             />
           </div>
         </div>
