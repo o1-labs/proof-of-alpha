a0724856e8bf7415dcc3b2fe32f56584a5b4ba34
ymekuria <ymekuria@gmail.com>
2022-01-26 19:40:21 -0800

    feat: add visible prop to button component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -3,20 +3,22 @@ import React, { ButtonHTMLAttributes } from 'react';
 interface buttonPropTypes extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonStyle?: string;
   label: string;
+  visable?: boolean;
 }
 /**
  * @param {string} buttonStyle Additional TailwindCSS classes (optional).
  * @param {string} label Button label
+ * @param {boolean} visable Boolean flag to show or hide Button (optional)
  */
 
 // Add icon logo prop
 const Button = (props: buttonPropTypes) => {
-  const { buttonStyle, label, ...otherProps } = props;
+  const { buttonStyle, label, visable, ...otherProps } = props;
 
   return (
     <button
-      className={`flex
-       justify-center line text-white font-normal leading-loose tracking-xlwidest items-center  px-1 py-1 2xl:px-3 2xl:py-2 3xl:py-3 3xl:px-4 bg-light-black hover:bg-opacity-75 active:text-custom-gray-button-border border border-white space-x-4 ${buttonStyle}`}
+      className={`${visable === false ? 'hidden' : 'flex'} 
+       justify-center line text-white font-normal leading-loose tracking-xlwidest items-center  px-1 py-1 2xl:px-3 2xl:py-2 3xl:py-3 3xl:px-4 bg-light-black hover:bg-opacity-75 active:text-custom-gray-button-border border border-white space-x-4  ${buttonStyle}`}
       {...otherProps}
     >
       {label}
