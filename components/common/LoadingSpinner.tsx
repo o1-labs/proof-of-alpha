26e6f09c99ce45d7c32c684772f9f0052b98e167
ymekuria <ymekuria@gmail.com>
2022-01-26 16:52:29 -0800

    fix: update loading spinner for different screen sizes

--- components/common/LoadingSpinner.tsx
+++ components/common/LoadingSpinner.tsx
@@ -18,11 +18,8 @@ const LoadingSpinner = ({ label }: loadingSpinnerPropTypes) => {
   };
 
   return (
-    <div className="relative flex flex-col bg-custom-gray-spinner-background w-96 h-96 items-center justify-center">
-      <div className="absolute  text-[#05ff00] text-center top-5/12 left-4/12">
-        {/* <p>labelWords[0]</p>
-        <p>ZERO-KNOWLEDGE</p>
-        <p>PROOF</p> */}
+    <div className="relative flex flex-col bg-custom-gray-spinner-background  w-[220px] h-[220px] 2xl:w-[270px] 2xl:h-[270px] items-center justify-center">
+      <div className="absolute  text-[#05ff00] text-center text-xs top-5/12 left-4/12 space-y-[.5px] 2xl:space-y-1 ">
         {renderLabel()}
       </div>
       <div className="loadingio-spinner-dual-ring-fv3bcitu1qf">
