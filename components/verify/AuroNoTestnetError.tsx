d8bd9163eeacbfa6210815c7196e814b44756bf5
ymekuria <ymekuria@gmail.com>
2022-01-28 21:34:55 -0800

    feat: add auro wallet error component styles

--- components/verify/AuroNoTestnetError.tsx
+++ components/verify/AuroNoTestnetError.tsx
@@ -1,17 +1,32 @@
 import Image from 'next/image';
 import ExclamationIcon from '../../public/assets/verify/finalize/exclamation-icon.png';
 
-const AuroNoTestnetError = () => {
+interface AuroNoTestNetErrorPropTypes {
+  visible?: boolean;
+}
+/**
+ * @param {boolean} visable Boolean flag to show or hide error (optional)
+ **/
+
+const AuroNoTestNetErrorPropTypes = ({
+  visible
+}: AuroNoTestNetErrorPropTypes) => {
   return (
-    <>
+    <div
+      className={`${
+        visible ? 'visable' : 'invisible'
+      } absolute bottom-[105px] right-72 2xl:bottom-32 2xl:right-[400px] 3xl:bottom-48 3xl:right-[700px]`}
+    >
       <div className="flex items-center justify-end  ">
-        <Image src={ExclamationIcon} alt="info" />
-        <span className="text-2xl leading-loose ml-2 ">
-          Auro Wallet must be connected to Snapps Testnet to continue.
+        <div className="w-3 2xl:w-auto">
+          <Image src={ExclamationIcon} alt="info" />
+        </div>
+        <span className="text-sm 2xl:text-2xl 3xl:text-3xl leading-loose ml-2 ">
+          Auro Wallet must be connected to Snappsnet to continue.
         </span>
       </div>
-    </>
+    </div>
   );
 };
 
-export default AuroNoTestnetError;
+export default AuroNoTestNetErrorPropTypes;
--- components/verify/AuroNoWalletError.tsx
+++ components/verify/AuroNoWalletError.tsx
@@ -1,38 +1,47 @@
 import Image from 'next/image';
 import ExclamationIcon from '../../public/assets/verify/finalize/exclamation-icon.png';
 import ExternalLinkIcon from '../../public/assets/common/external-link-icon.png';
-interface AuroNoTokensErrorPropTypes {
+
+interface AuroNoWalletErrorPropTypes {
   visible?: boolean;
 }
 /**
  * @param {boolean} visable Boolean flag to show or hide error (optional)
  **/
-const AuroNoTokensError = ({ visible }: AuroNoTokensErrorPropTypes) => {
+
+const AuroError = ({ visible }: AuroNoWalletErrorPropTypes) => {
   return (
     <div
       className={`${
         visible ? 'visable' : 'invisible'
       } absolute bottom-[105px] right-72 2xl:bottom-32 2xl:right-[400px] 3xl:bottom-48 3xl:right-[700px]`}
     >
-      <div className="flex items-center justify-end">
-        <div className="w-3 2xl:w-auto">
+      <div
+        className={`flex  items-center justify-end ${
+          visible ? 'visable' : 'invisible'
+        }  `}
+      >
+        <div className="w-3">
           <Image src={ExclamationIcon} alt="info" />
         </div>
+
         <span className="text-sm 2xl:text-2xl 3xl:text-3xl leading-loose ml-2 ">
-          Auro Wallet must have testnet tokens to continue.
+          Auro Wallet must be installed in your browser to continue.
         </span>
       </div>
-      <div className="flex justify-end">
+      <div className="flex items-center justify-end  ">
         <a
           className="flex"
           target="_blank"
-          href={'https://faucet.minaprotocol.com'}
+          href={
+            'https://chrome.google.com/webstore/detail/auro-walletmina-protocol/cnmamaachppnkjgnildpdmkaakejnhae'
+          }
         >
           <div className="flex items-center justify-center w-5 2xl:w-8 3xl:w-7">
             <Image src={ExternalLinkIcon} alt="info" />
           </div>
-          <span className="flex items-center text-xs 2xl:text-xl underline leading-loose ml-2 ">
-            MINA FAUCET
+          <span className="text-xs 2xl:text-xl underline leading-loose ml-2 ">
+            GET AURO
           </span>
         </a>
       </div>
@@ -40,4 +49,4 @@ const AuroNoTokensError = ({ visible }: AuroNoTokensErrorPropTypes) => {
   );
 };
 
-export default AuroNoTokensError;
+export default AuroError;
