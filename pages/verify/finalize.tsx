f5e6ceccd1374fa1a705206d9318f822621b8aaa
ymekuria <ymekuria@gmail.com>
2022-01-28 22:25:41 -0800

    feat: add method to display auro wallet errors

--- pages/verify/finalize.tsx
+++ pages/verify/finalize.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import type { NextPage } from 'next';
 import Button from '../../components/common/Button';
@@ -8,12 +8,21 @@ import LightButton from '../../components/common/LightButton';
 import ReturnsAndTradeBadge from '../../components/finalize/ReturnsAndTradeBadge';
 import SocialBadge from '../../components/finalize/SocialBadge';
 
-import AuroError from '../../components/verify/AuroError';
+import AuroNoWalletError from '../../components/verify/AuroNoWalletError';
 import AuroNoTestnetError from '../../components/verify/AuroNoTestnetError';
 import AuroNoTokensError from '../../components/verify/AuroNoTokensError';
 
 const Finalize: NextPage = () => {
   const [activeButton, setActiveButton] = useState<string>('returnsAndHistory');
+  const [isDisabled, setIsdisabled] = useState<boolean>(false);
+  useEffect(() => {
+    // if (
+    //   errorType === 'wallet' ||
+    //   errorType === 'testnet' ||
+    //   errorType === 'tokens'
+    // ) {
+    //   setIsdisabled(true);
+  }, []);
 
   const renderSocialBadges = (): React.ReactElement => {
     return activeButton === 'returnsAndHistory' ? (
@@ -25,6 +34,20 @@ const Finalize: NextPage = () => {
 
   const handleButtonSelection = (event: any): void => {
     setActiveButton(event.target.value);
+  };
+
+  const renderErrors = (errorType?: string) => {
+    switch (errorType) {
+      case 'wallet':
+        return <AuroNoWalletError visible={true} />;
+        break;
+      case 'testnet':
+        return <AuroNoTestnetError visible={true} />;
+      case 'tokens':
+        return <AuroNoTokensError visible={true} />;
+      default:
+        break;
+    }
   };
   return (
     <div className="min-h-screen bg-background bg-cover bg-no-repeat">
@@ -41,7 +64,6 @@ const Finalize: NextPage = () => {
             <p className="text-sm 2xl:text-lg 3xl:text-xl tracking-wider mt-2">
               FINALIZE SETTINGS
             </p>
-
             <p className="text-1.5xl 2xl:text-4xl 3xl:text-4xl font-medium ">
               How much do you want to disclose?
             </p>
@@ -61,14 +83,13 @@ const Finalize: NextPage = () => {
               </div>
             </div>
             {/* button selection */}
-
             <div className="flex items-center 2xl:items-left justify-center 2xl:justify-start mb-5">
               {renderSocialBadges()}
             </div>
-
             {/* end social badges */}
             {/* nav buttons */}
-            {/* <AuroNoTokensError /> */}
+            {/* "wallet " "testnet" 'tokens' */}
+            {renderErrors()}
             <div className="flex justify-between mt-10">
               <Link href="/verify/review" passHref>
                 <TransparentButton
@@ -80,6 +101,7 @@ const Finalize: NextPage = () => {
                 <Button
                   buttonStyle="w-3/12 text-sm 2xl:text-xl 3xl:text-2xl"
                   label="CONTINUE  >>"
+                  disabled={isDisabled}
                 />
               </Link>
             </div>
