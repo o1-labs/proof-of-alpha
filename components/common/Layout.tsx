e32879a45f00bfcb07e42830756e77a8f6bd9e2a
ymekuria <ymekuria@gmail.com>
2022-02-04 14:29:48 -0800

    feat: add background color prop to layout component

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,12 +1,30 @@
 import React, { ReactNode } from 'react';
 
-interface layoutpPropTypes {
+interface layoutPropTypes {
+  backGroundColor: 'gradient' | 'blue' | 'red';
+  layoutStyle?: string;
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: layoutpPropTypes) => {
+/**
+ * @param {string} backGroundColor Background color 'gradient' | 'blue' | 'red'
+ * @param {string} layoutStyle Additional TailwindCSS classes (optional).
+ *
+ **/
+
+const Layout = (props: layoutPropTypes) => {
+  const { backGroundColor, layoutStyle, children, ...otherProps } = props;
+  const backGroundColorClassName = {
+    gradient: 'bg-background',
+    blue: 'bg-blue-background',
+    red: 'bg-red-background'
+  };
+
   return (
-    <div className="min-h-screen bg-background bg-cover bg-no-repeat">
+    <div
+      className={` ${backGroundColorClassName[backGroundColor]} flex h-screen bg-cover bg-no-repeat text-white ${layoutStyle}`}
+      {...otherProps}
+    >
       {children}
     </div>
   );
