c1e9c0157adcbb4eb29737cbea156db6472c889e
ymekuria <ymekuria@gmail.com>
2022-02-06 20:07:30 -0800

    fix: change youll recieve font color to light gray

--- components/finalize/ReturnsAndTradeBadge.tsx
+++ components/finalize/ReturnsAndTradeBadge.tsx
@@ -5,8 +5,11 @@ import TwitterCardf from '../../public/assets/common/twitter-card.png';
 
 const ReturnsAndTradeBadge = () => {
   return (
-    <div className="flex flex-col items-center  justify-center w-9/12 2xl:w-auto ">
-      <p className=" flex text-xs mb-4 2xl:mt-6 2xl:mr-48 3xl:mr-72 2xl:text-xl  tracking-2xlwidest 2xl:tracking-2xlwidest">
+    <div className="flex w-9/12 flex-col  items-center justify-center 2xl:w-auto ">
+      <p
+        className="3xl:mr-72
+ tracking-2xlwidest 2xl:tracking-2xlwidest mb-4 flex text-xs text-[#BCBCBC] 2xl:mt-6  2xl:mr-48 2xl:text-xl"
+      >
         YOU'LL RECIEVE
       </p>
       <div className="flex justify-center 2xl:mt-4">
@@ -14,16 +17,16 @@ const ReturnsAndTradeBadge = () => {
           <div className="text-center text-xs 2xl:text-xl  ">
             PROOF OF AlPHA STATEMENT
           </div>
-          <div className="2xl:w-10/12 3xl:w-full">
+          <div className="3xl:w-full 2xl:w-10/12">
             <Image src={ProofOfTradeStatement} alt="proof of trade statement" />
           </div>
         </div>
 
         <div className="-ml-20 2xl:-ml-36">
-          <div className=" text-center 2xl:text-left 2xl:ml-28 2xl:mr-50 text-xs 2xl:text-xl  ">
+          <div className=" 2xl:mr-50 text-center text-xs 2xl:ml-28 2xl:text-left 2xl:text-xl  ">
             SOCIAL BADGE
           </div>
-          <div className="w-1/2  mt-2 2xl:mt-4 3xl:mt-7 ml-7 2xl:-ml-16 3xl:-ml-8 ">
+          <div className="3xl:mt-7  3xl:-ml-8 mt-2 ml-7 w-1/2 2xl:mt-4 2xl:-ml-16 ">
             <Image src={TwitterCardf} alt="twitter card" />
           </div>
         </div>
--- components/finalize/SocialBadge.tsx
+++ components/finalize/SocialBadge.tsx
@@ -3,14 +3,14 @@ import TwitterCard from '../../public/assets/verify/finalize/twitter-card-finali
 
 const ReturnsAndTradeBadge = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-9/12 2xl:full 2xl:ml-10">
-      <p className="flex text-xs mb-4 2xl:mt-6  2xl:text-xl  tracking-2xlwidest 2xl:tracking-2xlwidest">
+    <div className="2xl:full flex w-9/12 flex-col items-center justify-center 2xl:ml-10">
+      <p className="tracking-2xlwidest 2xl:tracking-2xlwidest mb-4 flex  text-xs text-[#BCBCBC]  2xl:mt-6 2xl:text-xl">
         YOU'LL RECIEVE
       </p>
-      <div className="text-center text-xs 2xl:text-xl 2xl:mt-4  ">
+      <div className="text-center text-xs 2xl:mt-4 2xl:text-xl  ">
         SOCIAL BADGE
       </div>
-      <div className="w-9/12 2xl:w-8/12 3xl:w-10/12 -mt-1 mb-2 2xl:mb-24 3xl:mb-30 2xl:-mt-2 ">
+      <div className="3xl:w-10/12 3xl:mb-30 -mt-1 mb-2 w-9/12 2xl:mb-24 2xl:-mt-2 2xl:w-8/12 ">
         <Image src={TwitterCard} alt="twitter card" />
       </div>
     </div>
