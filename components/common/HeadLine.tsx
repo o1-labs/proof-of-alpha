d547a20a86474db2f57c5d3822234880b1896efa
ymekuria <ymekuria@gmail.com>
2022-02-09 01:48:28 -0800

    feat: add prop for additional tailwind styles to headline and subheadline component

--- components/common/HeadLine.tsx
+++ components/common/HeadLine.tsx
@@ -1,17 +1,18 @@
 interface headLineProps {
   text: string;
+  headLineStyle?: string;
 }
 
 /**
  * @param {string} text Header large text
- *
+ * @param {string} headLineStyle Additional TailwindCSS classes (optional).
  **/
 
 const HeadLine = (props: headLineProps) => {
-  const { text, ...otherProps } = props;
+  const { text, headLineStyle, ...otherProps } = props;
   return (
     <h1
-      className="md:text-1.5xl 2xl:text-3.5xl font-medium md:pb-9 2xl:pb-12 2xl:pt-5"
+      className={`md:text-1.5xl 2xl:text-3.5xl font-medium md:pb-9 2xl:pb-12 2xl:pt-5 ${headLineStyle} `}
       {...otherProps}
     >
       {text}
--- components/common/SubHeadLine.tsx
+++ components/common/SubHeadLine.tsx
@@ -1,16 +1,20 @@
 interface subHeadLineProps {
   text: string;
+  subHeadLineStyle?: string;
 }
 
 /**
  * @param {string} text Header large text
- *
+ *@param {string} subHeadLineStyle
  **/
 
 const SubHeadLine = (props: subHeadLineProps) => {
-  const { text, ...otherProps } = props;
+  const { text, subHeadLineStyle, ...otherProps } = props;
   return (
-    <h2 className="mt-2 text-sm tracking-wider 2xl:text-lg" {...otherProps}>
+    <h2
+      className={`mt-2 text-sm tracking-wider 2xl:text-lg ${subHeadLineStyle}`}
+      {...otherProps}
+    >
       {text}
     </h2>
   );
