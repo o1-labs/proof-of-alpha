b4e6b06fc1dd05a4418f56571ab2c8255c9eba46
ymekuria <ymekuria@gmail.com>
2022-02-09 11:05:19 -0800

    fix: add children to button component

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -8,24 +8,25 @@ interface buttonPropTypes extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 /**
  * @param {string} buttonStyle Additional TailwindCSS classes (optional).
- * @param {string} label Button label
+ * @param {string} label Button label (optional)
  * @param {boolean} visable Boolean flag to show or hide Button (optional)
- * @param {boolean} secondary Boolean flag to to change primary button styles defaults to primary
+ * @param {boolean} secondary Boolean flag to to change primary button styles defaults to primary (optional)
  **/
 
-// Add icon logo prop
 const Button = (props: buttonPropTypes) => {
-  const { buttonStyle, label, visable, secondary, ...otherProps } = props;
+  const { buttonStyle, label, visable, secondary, children, ...otherProps } =
+    props;
 
   return (
     <button
       className={`${visable === false ? 'hidden' : 'flex'} ${
         secondary === true ? 'bg-[#FFFFFF1A]' : 'bg-light-black'
       } 
-       justify-center z-10 line text-white font-normal leading-loose tracking-xlwidest items-center  px-1 py-1 2xl:px-3 2xl:py-2 3xl:py-3 3xl:px-4  hover:bg-opacity-75 active:text-custom-gray-button-border border border-white space-x-4  ${buttonStyle}`}
+       line tracking-xlwidest 3xl:py-3 3xl:px-4 active:text-custom-gray-button-border z-10 items-center justify-center  space-x-4 border border-white px-1 py-1 font-normal  leading-loose text-white hover:bg-opacity-75 2xl:px-3 2xl:py-2  ${buttonStyle}`}
       {...otherProps}
     >
       {label}
+      {children}
     </button>
   );
 };
