3aae0a656f4c4e46e45768d1bd28dc3ccf77b548
ymekuria <ymekuria@gmail.com>
2022-02-15 14:03:05 -0800

    fix: add comma formatting to computed profit and loss percentages

--- utils/index.ts
+++ utils/index.ts
@@ -1,24 +1,30 @@
 import { Trade } from '../types';
 
 export const calculateCumulativeProfitLoss = (trades: Trade[]) => {
-  const cummulativeProfitLoss = trades
-    .reduce((trades, trade) => {
-      const tradeProfitLoss =
-        ((parseFloat(trade.sell.price) - parseFloat(trade.buy.price)) /
-          parseFloat(trade.buy.price)) *
-        100;
-      return trades + tradeProfitLoss;
-    }, 0)
-    .toFixed(1);
-  return cummulativeProfitLoss;
+  const cummulativeProfitLoss = trades.reduce((trades, trade) => {
+    const tradeProfitLoss =
+      ((parseFloat(trade.sell.price) - parseFloat(trade.buy.price)) /
+        parseFloat(trade.buy.price)) *
+      100;
+    return trades + tradeProfitLoss;
+  }, 0);
+
+  // This adds comma formating for larger numbers
+  return cummulativeProfitLoss.toLocaleString('en-us', {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1
+  });
 };
 
 export const calculateTradeProfitLoss = (trade: Trade) => {
-  const tradeProfitLoss = (
+  const tradeProfitLoss =
     ((parseFloat(trade.sell.price) - parseFloat(trade.buy.price)) /
       parseFloat(trade.buy.price)) *
-    100
-  ).toFixed(1);
+    100;
 
-  return tradeProfitLoss;
+  // This adds comma formating for larger numbers
+  return tradeProfitLoss.toLocaleString('en-us', {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1
+  });
 };
