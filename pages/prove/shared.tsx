9b45de7ca24a9de14fba9d7dad2de2a2285835fd
ymekuria <ymekuria@gmail.com>
2022-02-18 16:51:42 -0700

    fix: update footer on mobile prove output

--- pages/prove/shared.tsx
+++ pages/prove/shared.tsx
@@ -3,10 +3,7 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import Layout from '../../components/common/Layout';
 import Button from '../../components/common/Button';
-
 import ZKLogo from '../../public/assets/common/zk-logo.png';
-
-import ProofOfAlphaStatment from '../../public/assets/prove/proof-of-alpha-statement-prove.png';
 import ShieldIcon from '../../public/assets/prove/shield-icon.png';
 import ArrowIcon from '../../public/assets/prove/arrow-icon.png';
 import TransparentButton from '../../components/common/TransparentButton';
@@ -15,87 +12,92 @@ import MinaPrimary from '../../public/assets/prove/mina-icon-primary.png';
 
 import TopStamp from '../../public/assets/prove/prove-stamp.png';
 import ProofOfAlphaStatement from '../../components/prove/ProofOfAlphaStatement';
+import Container from '../../components/common/Container';
 
 const Shared: NextPage = () => {
   return (
-    <Layout backGroundColor="gray">
-      <div className=" 3xl:-top-56 3xl:w-5/12 absolute -top-36 right-0  w-7/12 2xl:-top-52 2xl:w-6/12">
+    <Layout
+      backGroundColor="gray"
+      layoutStyle="relative h-2.25*screen min-h-fit 2xl:h-1.75*screen"
+    >
+      <div className=" 3xl:-top-56 3xl:w-5/12 -top-30 absolute -right-52  md:-top-36 md:right-0 md:w-7/12 2xl:-top-52 2xl:w-6/12">
         <Image src={TopStamp} alt="Stamp" />
       </div>
-
-      <div className="ml-110 flex">
-        <div className="flex flex-col items-center ">
-          <div className="pt-4 md:w-20 2xl:w-20">
-            <Image src={ZKLogo} alt="zk logo" />
-          </div>
-          <div className="3xl:w-7/12 flex w-5/12 flex-col items-center justify-center 2xl:-mt-8 2xl:w-[570px]">
-            <ProofOfAlphaStatement />
-          </div>
-        </div>
-
-        <div className="3xl:mt-86 mt-64 w-3/12">
-          <div className="3xl:mb-28 flex flex-col items-center 2xl:mb-20">
-            <p className="3xl:text-lg tracking-wide 2xl:text-base">
-              GET YOUR FREE PROOF OF
-            </p>
-            <p className="3xl:text-lg 3xl:mb-6  2xl:mb-5 2xl:text-base">
-              ALPHA STATMENT
-            </p>
-            <Link href={'/prove/next'}>
-              <Button
-                label="GET IT NOW"
-                buttonStyle="text-xs w-5/12 2xl:w-5/12"
-              />
-            </Link>
-          </div>
-          <div className="3xl:space-y-3 flex flex-col items-center 2xl:space-y-1 ">
-            <div className="2xl:w-10">
-              <Image src={ShieldIcon} alt="shield icon" />
+      <Container containerStyle="">
+        <div className="mt-3 flex flex-col justify-between md:mt-0 md:flex-row">
+          <div className="flex flex-col items-center space-y-4 md:space-y-8">
+            <div className="3xl:w-28 w-18 pt-4 md:w-20">
+              <Image src={ZKLogo} alt="zk logo" />
             </div>
-            <p className="3xl:text-lg font-extrabold 2xl:text-base">
-              How do you know this proof is valid?
-            </p>
-            <p className="3xl:text-base 2xl:text-sm">
-              Transactions that are accepted by the Mina
-            </p>
-            <p className="3xl:text-base 2xl:text-sm">
-              blockchain are considered valid.
-            </p>
-            <a target="_blank" href={'https://minaexplorer.com/'}>
-              <div className="flex items-center">
-                <div className="3xl:w-3 2xl:w-2">
-                  <Image src={ArrowIcon} alt="arrow icon" />
-                </div>
-                <TransparentButton
-                  label="VIEW PROOF ON MINA EXPLORER"
-                  buttonStyle="text-[10px]  2xl:tracking-normal text-base 3xl:text-xs"
-                />
-              </div>
-            </a>
+            <div className="3xl:w-full mt-20 flex w-11/12 flex-col items-center justify-center md:mt-36 md:w-full  2xl:w-full">
+              <ProofOfAlphaStatement />
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div className="3xl:h-[350px] absolute bottom-0 h-[170px] w-full  bg-[#79797940] 2xl:h-[300px]">
-        <div className="flex flex-col  items-center  ">
-          <div className="3xl:w-3/12 3xl:space-y-10  flex w-4/12 flex-col items-center justify-center space-y-4 2xl:w-3/12 2xl:space-y-6 ">
-            <div className=" 3xl:w-auto 3xl:mt-18 mt-5  w-14 2xl:mt-12 2xl:w-14">
-              <Image src={MinaPrimary} alt="mina logo" />
-              <p className="3xl:text-xs text-[7px] text-[#FF603B] 2xl:pl-1">
-                MADE ON MINA
+          <div className="3xl:mt-86  mt-14 md:mt-64">
+            <div className="3xl:mb-28 mb-20 flex flex-col items-center 2xl:mb-20">
+              <p className="3xl:text-lg text-[.6em] leading-5 tracking-widest md:text-xs md:leading-none md:tracking-wide xl:text-sm">
+                GET YOUR FREE PROOF OF
               </p>
+              <p className="3xl:text-lg 3xl:mb-6 mb-4 text-[.6em] tracking-widest md:tracking-wide xl:text-sm 2xl:mb-5">
+                ALPHA STATMENT
+              </p>
+              <Link href={'/prove/next'}>
+                <Button
+                  label="GET IT NOW"
+                  buttonStyle="text-[.6em] md:text-xs 3xl:text-sm w-4/12 md:w-7/12 2xl:8/12 3xl:w-8/12"
+                />
+              </Link>
             </div>
-            <p className="3xl:text-3xl text-base tracking-wider 2xl:text-xl">
-              zero-knowledge proof-powered dApps
-            </p>
-            <a target="_blank" href="https://docs.minaprotocol.com/en/snapps">
-              <Button
-                label="LEARN MORE"
-                buttonStyle=" w-[160px] 2xl:w-[185px] 3xl:w-[210px] text-xs 2xl:text-base 3xl:text-base"
-                secondary={true}
-              />
-            </a>
+            <div className="3xl:space-y-3 flex flex-col items-center space-y-2 2xl:space-y-1 ">
+              <div className="w-8 lg:w-8 2xl:w-10">
+                <Image src={ShieldIcon} alt="shield icon" />
+              </div>
+              <p className="3xl:text-lg text-[.65em] font-medium md:text-xs  2xl:text-base">
+                How do you know this proof is valid?
+              </p>
+              <p className="3xl:text-base text-[.55em] md:text-xs 2xl:text-sm">
+                Transactions that are accepted by the Mina
+              </p>
+              <p className="3xl:text-base text-[.55em] md:text-xs 2xl:text-sm">
+                blockchain are considered valid.
+              </p>
+              <a target="_blank" href={'https://minaexplorer.com/'}>
+                <div className="flex items-center">
+                  <div className="3xl:w-3 w-2 2xl:w-2">
+                    <Image src={ArrowIcon} alt="arrow icon" />
+                  </div>
+                  <TransparentButton
+                    label="VIEW PROOF ON MINA EXPLORER"
+                    buttonStyle="text-[.4rem] md:text-[8px] text-[#C4C4C4]  2xl:tracking-normal 2xl:text-[11px] 3xl:text-xs"
+                  />
+                </div>
+              </a>
+            </div>
           </div>
+        </div>
+      </Container>
+
+      <div className="3xl:h-1/4 absolute  bottom-0 mt-20 h-80  w-full bg-[#79797940]   md:h-1/6 lg:h-52  xl:h-72 2xl:h-1/4">
+        <div className="3xl:space-y-8 flex h-full  flex-col items-center justify-center space-y-5  2xl:space-y-9 ">
+          <div className=" flex   flex-col items-center ">
+            <div className="3xl:w-auto flex w-10 items-center justify-center xl:w-12 2xl:w-16">
+              <Image src={MinaPrimary} alt="mina logo" />
+            </div>
+            <p className="3xl:text-base tracking-xlwidest 3xl:mt-1 mt-1 w-fit text-center text-[.4rem] text-[#FF603B] md:text-[.44rem] 2xl:text-[10px] ">
+              MADE ON MINA
+            </p>
+          </div>
+          <p className="2xl:text-1.5xl 3xl:text-3xl 3xl:pb-3 w-7/12 pb-3 text-center text-base tracking-widest md:w-full ">
+            zero-knowledge proof-powered dApps.
+          </p>
+          <a target="_blank" href="https://docs.minaprotocol.com/en/snapps">
+            <Button
+              label="LEARN MORE"
+              buttonStyle="w-36  mb-10 md:mb-0 2xl:w-48  3xl:w-52 text-[.6em] md:text-xs 2xl:text-base 3xl:text-base"
+              secondary={true}
+            />
+          </a>
         </div>
       </div>
     </Layout>
