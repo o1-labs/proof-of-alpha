e16aa73d6a6a8fa227fcb5e8b5e3d40453dc31e1
ymekuria <ymekuria@gmail.com>
2022-02-25 09:57:58 -0800

    fix: increase image sizes on medium screens' '

--- components/finalize/ReturnsAndTradeBadge.tsx
+++ components/finalize/ReturnsAndTradeBadge.tsx
@@ -1,33 +1,32 @@
 import Image from 'next/image';
 import ProofOfTradeStatement from '../../public/assets/verify/finalize/proof-of-trade-statement.png';
 import TwitterCard from '../../public/assets/verify/finalize/twitter-card-finalize.png';
-import TwitterCardf from '../../public/assets/common/twitter-card.png';
 
 const ReturnsAndTradeBadge = () => {
   return (
-    <div className="flex w-9/12 flex-col  items-center justify-center 2xl:w-auto ">
+    <div className="flex w-9/12 flex-col  items-center justify-center 2xl:mt-5 2xl:w-auto">
       <p
-        className="3xl:mr-72
- tracking-2xlwidest 2xl:tracking-2xlwidest mb-4 flex text-xs text-[#BCBCBC] 2xl:mt-6  2xl:mr-48 2xl:text-xl"
+        className="
+ tracking-2xlwidest 2xl:tracking-2xlwidest 3xl:mr-8  mb-12 mt-6 flex text-xs text-[#BCBCBC]  2xl:mt-6 2xl:mr-8 2xl:text-xl"
       >
         YOU'LL RECIEVE
       </p>
-      <div className="flex justify-center 2xl:mt-4">
-        <div className="">
-          <div className="text-center text-xs 2xl:text-xl  ">
-            PROOF OF AlPHA STATEMENT
+      <div className="3xl:-ml-40 flex justify-center 2xl:mt-4">
+        <div className="3xl:ml-30">
+          <div className="tracking-xlwidest text-center text-xs 2xl:text-xl ">
+            PROOF OF ALPHA STATEMENT
           </div>
-          <div className="3xl:w-full 2xl:w-10/12">
+          <div className="3xl:w-[700px] 3xl:-mt-9 2xl:-mt-4 xl:-mt-4 2xl:w-[600px] xl:w-[500px] ">
             <Image src={ProofOfTradeStatement} alt="proof of trade statement" />
           </div>
         </div>
 
-        <div className="-ml-20 2xl:-ml-36">
-          <div className=" 2xl:mr-50 text-center text-xs 2xl:ml-28 2xl:text-left 2xl:text-xl  ">
+        <div className="3xl:-ml-30 -ml-20 2xl:-ml-36  ">
+          <div className="tracking-xlwidest  text-center text-xs 2xl:ml-28 mr-20  2xl:text-left 2xl:text-xl  ">
             SOCIAL BADGE
           </div>
-          <div className="3xl:mt-7  3xl:-ml-8 mt-2 ml-7 w-1/2 2xl:mt-4 2xl:-ml-16 ">
-            <Image src={TwitterCardf} alt="twitter card" />
+          <div className="3xl:-mt-1 3xl:w-[530px] 3xl:-ml-20 xl:-ml-24 mt-0 ml-7 w-1/2 2xl:mt-0 2xl:-ml-16 2xl:w-[500px] xl:w-[400px]">
+            <Image src={TwitterCard} alt="twitter card" />
           </div>
         </div>
       </div>
--- components/finalize/SocialBadge.tsx
+++ components/finalize/SocialBadge.tsx
@@ -3,14 +3,14 @@ import TwitterCard from '../../public/assets/verify/finalize/twitter-card-finali
 
 const ReturnsAndTradeBadge = () => {
   return (
-    <div className="2xl:full flex w-9/12 flex-col items-center justify-center 2xl:ml-10">
-      <p className="tracking-2xlwidest 2xl:tracking-2xlwidest mb-4 flex  text-xs text-[#BCBCBC]  2xl:mt-6 2xl:text-xl">
+    <div className="2xl:full flex w-9/12 xl:w-11/12 flex-col items-center justify-center">
+      <p className="tracking-2xlwidest mb-14 flex pt-5 text-xs  text-[#BCBCBC] 2xl:mt-6  2xl:text-xl 2xl:tracking-[10px]">
         YOU'LL RECIEVE
       </p>
-      <div className="text-center text-xs 2xl:mt-4 2xl:text-xl  ">
+      <div className="tracking-xlwidest  -mb-5 text-center text-xs 2xl:mt-4 2xl:text-xl">
         SOCIAL BADGE
       </div>
-      <div className="3xl:w-10/12 3xl:mb-30 -mt-1 mb-2 w-9/12 2xl:mb-24 2xl:-mt-2 2xl:w-8/12 ">
+      <div className="3xl:w-10/12 3xl:mb-30  w-9/12 2xl:mb-24 2xl:-mt-2 2xl:w-full xl:w-full ">
         <Image src={TwitterCard} alt="twitter card" />
       </div>
     </div>
