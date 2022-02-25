2fd54b4cb73c68a20f9535d626f82dabd76a946c
ymekuria <ymekuria@gmail.com>
2022-02-25 09:52:19 -0800

    feat: show loading spinner before proof is confirmed

--- pages/verify/generate.tsx
+++ pages/verify/generate.tsx
@@ -11,9 +11,10 @@ import TradeData from '../../trades.json';
 import { Trade } from '../../types';
 import { calculateCumulativeProfitLoss } from '../../utils';
 import Layout from '../../components/common/Layout';
+import Container from '../../components/common/Container';
 
 const Generate: NextPage = () => {
-  const [proofConfirmed, setProofConfirmed] = useState<boolean>(true);
+  const [proofConfirmed, setProofConfirmed] = useState<boolean>(false);
   const [trades, setTrades] = useState<Trade[]>([]);
   const [cumulativeProfits, setCumulativeProfits] = useState<string>('');
   useEffect(() => {
@@ -25,9 +26,13 @@ const Generate: NextPage = () => {
   useEffect(() => {
     async function fetchProofStatus() {
       // fetch status and setProofConfirmed to true on success => setProofConfirmed(true)
+      setTimeout(() => {
+        setProofConfirmed(true);
+      }, 500);
     }
+    fetchProofStatus();
     return () => {
-      // cleanup
+      clearTimeout();
     };
   }, []);
 
@@ -40,57 +45,61 @@ const Generate: NextPage = () => {
   };
 
   return (
-    <Layout backGroundColor="gradient">
-      <div className="container flex  h-screen justify-center">
-        <div className="3xl:mt-24 mt-10 flex flex-col space-y-6 text-white 2xl:mt-20 2xl:space-y-10">
+    <Layout backGroundColor="gradient" layoutStyle="h-1.25*screen">
+      <div className=" flex  h-screen justify-center">
+        <div className="3xl:mt-36 mt-10 flex flex-col space-y-6 text-white 2xl:mt-20 2xl:space-y-10">
           <div className="flex justify-center">{renderProofStatus()}</div>
-          <div className="3xl:text-4xl flex justify-center text-2xl 2xl:my-11 2xl:text-3xl">
+          <div className="3xl:text-4xl 3xl:pb-3 flex justify-center pt-4 text-2xl 2xl:my-11 2xl:text-3xl">
             #ZeroKnowledge
           </div>
           <div>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-light 2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-extralight 2xl:text-2xl">
               You're generating a zero-knowledge proof revealing
             </p>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-bold 2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-normal 2xl:text-2xl">
               {cumulativeProfits}% cumulative returns.
             </p>
           </div>
           <div>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-bold 2xl:text-xl">
+            <p className="3xl:text-3xl 3xl:leading-11 ffont-normal text-base 2xl:text-xl">
               The proof is generated locally in your web browser
             </p>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-light 2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-extralight 2xl:text-2xl">
               and won't reveal your private data (trade size,
             </p>
 
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-light  2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-extralight  2xl:text-2xl">
               account balance, personal information, etc).
             </p>
           </div>
           <div>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-bold 2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-normal 2xl:text-2xl">
               Your proof will be added to Mina's chain, then anyone
             </p>
-            <p className="3xl:text-3xl 3xl:leading-11 text-base font-light  2xl:text-2xl">
+            <p className="3xl:text-3xl 3xl:leading-11 text-base font-extralight  2xl:text-2xl">
               will be able to verify your returns.
             </p>
           </div>
-          <div className="p flex justify-center">
+          <div className="flex justify-center pt-10">
             {/* Buttton is only visable after a proof is generated */}
             <Link href={'/verify/confirm'}>
               <Button
-                buttonStyle="w-10/12 2xl:w-7/12 py-2 3xl:w-5/12 text-sm 2xl:text-base 3xl:text-base"
+                buttonStyle="w-10/12 2xl:w-7/12 py-2 3xl:w-6/12 text-sm 2xl:text-base 3xl:text-base"
                 visable={proofConfirmed}
               >
                 <div className="flex w-4 items-center">
                   <Image src={LinkIcon} />
                 </div>
-                <div className="px-4">SEND TO MINA {'>>'}</div>
+                <div className="px-4">
+                  SEND TO MINA &nbsp;
+                  {'>>'}
+                </div>
               </Button>
             </Link>
           </div>
         </div>
       </div>
+      {/* </Container> */}
     </Layout>
   );
 };
