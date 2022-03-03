759a511f8e6dcb0029abdcde2c240ba84a369655
ymekuria <ymekuria@gmail.com>
2022-03-03 11:57:13 -0800

    feat: expose account display showing first 6 and last 4 characters

--- hooks/useMinaProvider.ts
+++ hooks/useMinaProvider.ts
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 export default () => {
   const [isAuro, setIsAuro] = useState(false);
   const [minaAccount, setMinaAccount] = useState('');
+  const [accountHeaderDisplay, setAccountHeaderDisplay] =
+    useState('0xABCD...1234');
   useEffect(() => {
     async function getAccount() {
       let minaAccounts;
@@ -14,15 +16,22 @@ export default () => {
         }
         setIsAuro(true);
 
-        minaAccounts = await window?.mina.requestAccounts();
+        minaAccounts = await window.mina.requestAccounts();
         setMinaAccount(minaAccounts[0]);
-        console.log('minaaccounts', minaAccounts[0]);
+
+        // String showing only first 6 and last 4 characters of account for the header display
+        const display = `${minaAccounts[0].slice(
+          0,
+          6
+        )}...${minaAccounts[0].slice(-4)}`;
+        setAccountHeaderDisplay(display);
       } catch (error) {
-        console.log(error);
+        // if user reject, requestAccounts will throw an error with code and message filed
+        console.log(error.message, error?.code);
       }
     }
     getAccount();
-  }, []);
+  }, [isAuro, minaAccount, accountHeaderDisplay]);
 
-  return [isAuro, minaAccount];
+  return [isAuro, minaAccount, accountHeaderDisplay];
 };
