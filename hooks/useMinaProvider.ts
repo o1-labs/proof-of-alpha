5af1c127b4cb923b183de43ceb2b6551586af29b
ymekuria <ymekuria@gmail.com>
2022-03-03 10:27:31 -0800

    feat: detect if user is connected to auro wallet

--- hooks/useMinaProvider.ts
+++ hooks/useMinaProvider.ts
@@ -1,2 +1,22 @@
 import React, { useEffect, useState } from 'react';
-export default () => {};
+export default () => {
+  const [isAuro, setIsAuro] = useState(false);
+  useEffect(() => {
+    async function getAccount() {
+      try {
+        // No Auro wallet exists
+        if (!window.mina) {
+          // Redundate
+          setIsAuro(false);
+          return;
+        }
+        setIsAuro(true);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAccount();
+  }, []);
+
+  return [isAuro];
+};
