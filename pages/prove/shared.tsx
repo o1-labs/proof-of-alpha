a3275ffe0b8f8b85f9d61cf1697d6b4605c96629
ymekuria <ymekuria@gmail.com>
2022-03-16 15:01:21 -0700

    refactor: move user ed copy to seperate infopanel component

--- pages/prove/shared.tsx
+++ pages/prove/shared.tsx
@@ -9,10 +9,9 @@ import ShieldIcon from '../../public/assets/prove/shield-icon.png';
 import ArrowIcon from '../../public/assets/prove/arrow-icon.png';
 import TransparentButton from '../../components/common/TransparentButton';
 
-import MinaPrimary from '../../public/assets/prove/mina-icon-primary.png';
-
 import TopStamp from '../../public/assets/prove/prove-stamp.png';
 import ProofOfAlphaStatement from '../../components/prove/ProofOfAlphaStatement';
+import InfoPanel from '../../components/prove/InfoPanel';
 import Container from '../../components/common/Container';
 import {
   addQueryParamsToURL,
@@ -58,48 +57,7 @@ const Shared: NextPage = () => {
               <ProofOfAlphaStatement />
             </div>
           </div>
-
-          <div className="mt-14  md:mt-64 2xl:mx-auto 3xl:mx-0 3xl:mt-86">
-            <div className="mb-20 flex flex-col items-center 2xl:mb-20 3xl:mb-28">
-              <p className="text-[.6em] leading-5 tracking-widest md:text-xs md:leading-none md:tracking-wide xl:text-sm 3xl:text-lg">
-                GET YOUR FREE PROOF OF
-              </p>
-              <p className="mb-4 text-[.6em] tracking-widest md:tracking-wide xl:text-sm 2xl:mb-5 3xl:mb-6 3xl:text-lg">
-                ALPHA STATMENT
-              </p>
-              <Link href={'/prove/next'} passHref>
-                <Button
-                  label="GENERATE IT NOW"
-                  buttonStyle="text-[.6em] md:text-[.5em] 3xl:text-xs w-4/12 md:w-8/12  hover:bg-white hover:bg-opacity-10"
-                />
-              </Link>
-            </div>
-            <div className="flex flex-col items-center space-y-2 2xl:space-y-1 3xl:space-y-3 ">
-              <div className="w-8 lg:w-8 2xl:w-10">
-                <Image src={ShieldIcon} alt="shield icon" />
-              </div>
-              <p className="text-[.65em] font-medium md:text-xs 2xl:text-base  3xl:text-lg">
-                How do you know this proof is valid?
-              </p>
-              <p className="text-[.55em] md:text-xs 2xl:text-sm 3xl:text-base">
-                Transactions that are accepted by the Mina
-              </p>
-              <p className="text-[.55em] md:text-xs 2xl:text-sm 3xl:text-base">
-                blockchain are considered valid.
-              </p>
-              <a target="_blank" href={'https://minaexplorer.com/'}>
-                <div className="flex items-center">
-                  <div className="w-2 2xl:w-2 3xl:w-3">
-                    <Image src={ArrowIcon} alt="arrow icon" />
-                  </div>
-                  <TransparentButton
-                    label="VIEW PROOF ON MINA EXPLORER"
-                    buttonStyle="text-[.4rem] md:text-[8px] text-[#C4C4C4]  2xl:tracking-normal 2xl:text-[11px] 3xl:text-xs hover:underline"
-                  />
-                </div>
-              </a>
-            </div>
-          </div>
+          <InfoPanel />
         </div>
       </Container>
 
