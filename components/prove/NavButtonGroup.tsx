e351fd3200c17ad2dedbc93b68c0eff1bd141503
ymekuria <ymekuria@gmail.com>
2022-03-16 12:48:03 -0700

    feat: add hover opacity to nav buttons

--- components/prove/NavButtonGroup.tsx
+++ components/prove/NavButtonGroup.tsx
@@ -7,16 +7,17 @@ const NavButtonGroup = ({
   activeButton,
   setActiveButton
 }: navButtonGroupProps) => {
+  console.log('button active', activeButton);
   return (
-    <div className="3xl:top-52 3xl:-left-86 absolute top-24 left-14 2xl:top-36 2xl:-left-36">
+    <div className="absolute top-24 left-14 2xl:top-36 2xl:-left-36 3xl:top-52 3xl:-left-86">
       <a href="#social">
         <div
           onClick={(event) => {
             event.preventDefault;
             setActiveButton('socialBadge');
           }}
-          className={` 3xl:w-[280px] 3xl:h-[48px] mb-1 flex h-[38px] w-[180px] items-center bg-[#FFFFFF1A] py-4 pl-5 text-xs font-extralight text-white 2xl:h-[48px] 2xl:w-[280px] ${
-            activeButton === 'socialBadge' ? 'border-l-2 ' : ''
+          className={` mb-1 flex h-[38px] w-[180px] items-center bg-[#FFFFFF1A] py-4 pl-5 text-xs font-extralight text-white  hover:bg-white hover:bg-opacity-20 2xl:h-[48px] 2xl:w-[280px] 3xl:h-[48px] 3xl:w-[280px] ${
+            activeButton === 'socialBadge' ? 'border-l-4 ' : 'border-l-0'
           }`}
         >
           SOCIAL BADGE
@@ -28,8 +29,8 @@ const NavButtonGroup = ({
             event.preventDefault;
             setActiveButton('tradeStatement');
           }}
-          className={`3xl:w-[280px] 3xl:h-[48px] mb-1 flex h-[38px] w-[180px] items-center bg-[#FFFFFF1A] py-4 pl-5 text-xs font-extralight text-white   2xl:h-[48px] 2xl:w-[280px] ${
-            activeButton == 'tradeStatement' ? 'border-l-2' : ''
+          className={`mb-1 flex h-[38px] w-[180px] items-center bg-[#FFFFFF1A] py-4 pl-5 text-xs font-extralight text-white  hover:bg-white hover:bg-opacity-20  2xl:h-[48px] 2xl:w-[280px]   3xl:h-[48px] 3xl:w-[280px] ${
+            activeButton == 'tradeStatement' ? 'border-l-4' : 'border-l-0'
           } `}
         >
           TRADE STATEMENT
