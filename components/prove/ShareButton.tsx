76d1b4d842ad526bbbdf77207396b5783d1204ca
ymekuria <ymekuria@gmail.com>
2022-03-17 00:14:51 -0700

    fix: update share it button layout on medium size screens

--- components/prove/ShareButton.tsx
+++ components/prove/ShareButton.tsx
@@ -19,16 +19,18 @@ const ShareButton = (props: shareButtonPropTypes) => {
   return (
     <button
       className={`
-        relative w-60  items-center justify-center space-x-4 rounded border border-white bg-light-black px-0 py-1 pr-6 text-xs font-normal   leading-loose tracking-xlwidest text-white hover:bg-opacity-75 active:text-custom-gray-button-border  xl:px-3  xl:py-2 2xl:px-3 2xl:py-2   2xl:pr-7 3xl:py-2 3xl:px-4 3xl:pr-7 3xl:text-base  ${shareButtonStyle}`}
+        relative w-60  items-center justify-center space-x-4 rounded border border-white bg-light-black px-3 py-2 text-xs font-normal leading-loose   tracking-xlwidest text-white hover:bg-opacity-75 active:text-custom-gray-button-border md:pr-6  xl:px-3  xl:py-2  2xl:py-2   2xl:pr-7  3xl:px-4  3xl:text-base  ${shareButtonStyle}`}
       {...otherProps}
     >
       <div className="flex flex-col">
         <p className="text-left text-sm">SHARE IT</p>
 
-        <p className="-pl-2  text-left text-[8px]">http://urlz.here/3dfdGs</p>
+        <p className="-pl-2  text-left md:text-[6px] xl:text-[8px]">
+          http://urlz.here/3dfdGs
+        </p>
       </div>
       <div
-        className="absolute right-7 top-2 w-5 xl:top-4 xl:right-5  2xl:right-4 2xl:top-3 2xl:w-7 3xl:right-5 3xl:top-3 3xl:w-7"
+        className=" absolute right-3 top-3 w-6  2xl:right-4 2xl:w-7 3xl:right-5  "
         aria-hidden="true"
       >
         <Image src={ShareIcon} alt="share icon" />
