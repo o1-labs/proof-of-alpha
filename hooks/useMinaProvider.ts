756436f432f26c56adaea5854e85d6e1879229aa
ymekuria <ymekuria@gmail.com>
2022-03-18 01:07:08 -0700

    feat: handle caseses when auro wallet is installed but no account and no wallet installed

--- hooks/useMinaProvider.ts
+++ hooks/useMinaProvider.ts
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+type statusType = 'green' | 'yellow' | 'red' | '';
 export default () => {
   const [isAuro, setIsAuro] = useState(false);
   const [minaAccount, setMinaAccount] = useState('');
+  const [status, setStatus] = useState<statusType>('');
   const [accountHeaderDisplay, setAccountHeaderDisplay] = useState('');
   useEffect(() => {
     async function getAccount() {
@@ -11,26 +13,38 @@ export default () => {
         if (!window.mina) {
           // Redundate
           setIsAuro(false);
+          setStatus('red');
+          setAccountHeaderDisplay('Not Connected');
           return;
         }
+
         setIsAuro(true);
 
         minaAccounts = await window.mina.requestAccounts();
-        setMinaAccount(minaAccounts[0]);
 
-        // String showing only first 6 and last 4 characters of account for the header display
-        const display = `${minaAccounts[0].slice(
-          0,
-          6
-        )}...${minaAccounts[0].slice(-4)}`;
-        setAccountHeaderDisplay(display);
+        // If window.mina exists but no accounts are returned, Auro wallet is installed but not connected to an account
+        if (minaAccounts.length === 0) {
+          setAccountHeaderDisplay('Not Connected');
+          setStatus('yellow');
+        } else {
+          // Auro is installed and connected to an account
+          // String showing only first 6 and last 4 characters of account for the header display
+          // TODO: Handle case when user has multiple accounts.
+          setMinaAccount(minaAccounts[0]);
+          const display = `${minaAccounts[0].slice(
+            0,
+            6
+          )}...${minaAccounts[0].slice(-4)}`;
+          setAccountHeaderDisplay(display);
+          setStatus('green');
+        }
       } catch (error) {
         // if user reject, requestAccounts will throw an error with code and message filed
         console.log(error.message, error?.code);
       }
     }
     getAccount();
-  }, [isAuro, minaAccount, accountHeaderDisplay]);
+  }, [isAuro, minaAccount, accountHeaderDisplay, status]);
 
-  return [isAuro, minaAccount, accountHeaderDisplay];
+  return [isAuro, minaAccount, accountHeaderDisplay, status];
 };
