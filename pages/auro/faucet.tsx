4bebcd0c29e774745c61c97e701ce31f3b5e4162
ymekuria <ymekuria@gmail.com>
2022-03-21 13:27:45 -0700

    feat: add query param to pre-populate mina address on the faucet

--- pages/auro/faucet.tsx
+++ pages/auro/faucet.tsx
@@ -8,21 +8,23 @@ import HeadLine from '../../components/common/HeadLine';
 import SubHeadLine from '../../components/common/SubHeadLine';
 import Layout from '../../components/common/Layout';
 import AuroBottomNavButtons from '../../components/auro/AuroBottomNavButtons';
+import useMinaProvider from '../../hooks/useMinaProvider';
 
 const Faucet: NextPage = () => {
+  const { minaAccount } = useMinaProvider();
   return (
     <Layout backGroundColor="gradient">
       <Header />
 
       <div className="flex flex-col items-center">
-        <div className="3xl:w-6/12 container  flex h-screen w-7/12 flex-col  md:w-6/12  2xl:w-7/12">
+        <div className="container flex  h-screen w-7/12 flex-col md:w-6/12  2xl:w-7/12  3xl:w-6/12">
           <ProgressBar stage="3rd" />
 
           <div className="flex h-screen flex-col  text-white md:space-y-4 2xl:space-y-10">
             <SubHeadLine text="GET TESTNET TOKENS" />
 
             <HeadLine text="Get free testnet MINA (tMINA) from Mina's faucet " />
-            <p className="3xl:text-3xl 2xl:text-1.5xl  pb-3 font-light md:text-sm">
+            <p className="pb-3 font-light  md:text-sm 2xl:text-1.5xl 3xl:text-3xl">
               Select the Berkeley network
             </p>
             <div className="h-auto">
@@ -32,7 +34,7 @@ const Faucet: NextPage = () => {
             <AuroBottomNavButtons
               backNavLink="/auro/install"
               externalButton
-              externalButtonNavLink="https://faucet.minaprotocol.com"
+              externalButtonNavLink={`https://faucet.minaprotocol.com?address=${minaAccount}`}
               externalButtonLabel="VISIT FAUCET"
               continueNavLink="/verify"
             />
