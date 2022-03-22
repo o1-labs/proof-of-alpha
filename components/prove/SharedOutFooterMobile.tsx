16535f4dfe8b01ab0a066f7ff1d274dc3484e5a1
ymekuria <ymekuria@gmail.com>
2022-03-22 13:22:47 -0700

    feat: add mobile footer component boilerplate

---
+++ components/prove/SharedOutFooterMobile.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import Button from '../common/Button';
+import MinaPrimary from '../../public/assets/prove/mina-icon-primary.png';
+
+const SharedOutFooterMobile = () => {
+  return <div></div>;
+};
+
+export default SharedOutFooterMobile;
