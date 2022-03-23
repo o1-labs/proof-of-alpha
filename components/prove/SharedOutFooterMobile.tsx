a51e038ff2b7e5e837a507eb1dce974774a0191b
ymekuria <ymekuria@gmail.com>
2022-03-23 11:00:38 -0700

    fix: increase mobile footer height and content size

--- components/prove/SharedOutFooterMobile.tsx
+++ components/prove/SharedOutFooterMobile.tsx
@@ -4,30 +4,31 @@ import MinaPrimary from '../../public/assets/prove/mina-icon-primary.png';
 
 const SharedOutFooterMobile = () => {
   return (
-    <div className="fixed bottom-0 z-50 h-16 w-full bg-[#79797940]/75 md:hidden">
+    <div className="fixed bottom-0 z-40 h-30 w-full bg-[#79797940]/[.9] md:hidden">
       <div className="flex h-full items-center justify-around">
         <div className="flex space-x-1">
-          <div className="flex w-7">
+          <div className="flex w-9">
             <Image src={MinaPrimary} alt="mina logo" />
           </div>
-          <div className="flex flex-col text-[.2rem] tracking-xlwidest text-[#FF603B]">
-            <p className="mt-1   text-center">MADE</p>
-            <p className="mt-1  text-center">ON</p>
-            <p className="mt-1  text-center">MINA</p>
+          <div className="flex flex-col space-y-1  text-center text-[.25rem] tracking-xlwidest text-[#FF603B]">
+            <p className="mt-[.3rem]">MADE</p>
+            <p>ON</p>
+            <p>MINA</p>
           </div>
         </div>
 
-        <p className="w-3/12 text-center  text-[.5rem] tracking-widest  ">
+        <p className="w-3/12 text-center  text-[.6rem] tracking-widest  ">
           Zero-knowledge zkApps.
         </p>
         <a
+          className="z-50"
           target="_blank"
           href="https://docs.minaprotocol.com/en/zkapps
           "
         >
           <Button
             label="LEARN MORE"
-            buttonStyle="w-24  text-[.4rem]  "
+            buttonStyle="w-28  text-[.5rem]"
             secondary={true}
           />
         </a>
