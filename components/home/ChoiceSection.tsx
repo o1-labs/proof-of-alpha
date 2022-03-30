e4559d476e9616a2f4b27a603eb3173146f0ec26
ymekuria <ymekuria@gmail.com>
2022-03-30 16:40:19 -0700

    refactor: create component for homepage choice section copy

---
+++ components/home/ChoiceSection.tsx
@@ -0,0 +1,22 @@
+const ChoiceSection = () => {
+  return (
+    <div className="mx-auto w-11/12 md:mx-0 md:mb-32 md:w-auto">
+      <p className="mb-14 text-center  text-xl  md:text-left lg:text-4xl 2xl:mb-14 3xl:mb-16 3xl:text-6xl">
+        The Choice is Yours
+      </p>
+
+      <p className="mb-5 text-base leading-loose md:leading-11 xl:text-2xl 2xl:mb-8 3xl:mb-10 3xl:text-4xl ">
+        Certify your entire account history, or a recent time period.
+      </p>
+      <p className="text-base leading-loose md:leading-11 xl:text-2xl 2xl:mb-10 3xl:mb-12 3xl:text-4xl">
+        Focus only on your sick gains, or share fully transparent transaction
+        data.
+      </p>
+      <p className="my-4 text-base leading-loose md:leading-11 xl:text-2xl 3xl:text-4xl ">
+        It's up to you.
+      </p>
+    </div>
+  );
+};
+
+export default ChoiceSection;
--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import type { NextPage } from 'next';
 import Layout from '../../components/common/Layout';
 import Button from '../../components/common/Button';
 import Indicator from '../../components/home/Indicator';
-
+import ChoiceSection from '../../components/home/ChoiceSection';
 import ArrowButton from '../../components/common/ArrowButton';
 import Container from '../../components/common/Container';
 
@@ -17,23 +16,9 @@ const Learn: NextPage = () => {
             <div className="relative mb-14   flex w-14 items-center  md:mb-0 md:h-40 md:w-40">
               <ArrowButton arrowDirection="left" navLink="/#exchange" />
             </div>
-            <div className="3xl:w-115 w-[275px] flex-col md:space-y-11 xl:w-[400px] 2xl:w-[420px]">
-              <div className="mx-auto w-11/12 md:mx-0 md:mb-32 md:w-auto">
-                <p className="3xl:text-6xl 3xl:mb-16  mb-14  text-center text-xl md:text-left lg:text-4xl 2xl:mb-14">
-                  The Choice is Yours
-                </p>
+            <div className="w-[275px] flex-col md:space-y-11 xl:w-[400px] 2xl:w-[420px] 3xl:w-115">
+              <ChoiceSection />
 
-                <p className="md:leading-11 3xl:text-4xl 3xl:mb-10 mb-5 text-base leading-loose xl:text-2xl 2xl:mb-8 ">
-                  Certify your entire account history, or a recent time period.
-                </p>
-                <p className="md:leading-11 3xl:text-4xl 3xl:mb-12 text-base leading-loose xl:text-2xl 2xl:mb-10">
-                  Focus only on your sick gains, or share fully transparent
-                  transaction data.
-                </p>
-                <p className="md:leading-11 3xl:text-4xl my-4 text-base leading-loose xl:text-2xl ">
-                  It's up to you.
-                </p>
-              </div>
               <div className="mt-24 flex flex-col items-center justify-center space-y-9 pr-10 md:mt-40 md:flex-row md:justify-between md:space-y-0">
                 <Link href={'/start'} passHref>
                   <Button
