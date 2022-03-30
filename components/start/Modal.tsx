a09b93f0521371276f25d220fb2056907ab0867a
ymekuria <ymekuria@gmail.com>
2022-03-30 13:30:30 -0700

    refactor: add retina get started view hero image

--- components/start/Modal.tsx
+++ components/start/Modal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, Dispatch, SetStateAction } from 'react';
+import { Fragment, Dispatch, SetStateAction } from 'react';
 
 import { Dialog, Transition } from '@headlessui/react';
 import { XIcon } from '@heroicons/react/outline';
