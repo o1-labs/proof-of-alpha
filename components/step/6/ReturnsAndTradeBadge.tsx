63caabf7b76f972e75f5d182a5d1e9587383cf93
ymekuria <ymekuria@gmail.com>
2022-04-07 13:05:39 -0700

    feat: replace hero images

--- components/step/6/ReturnsAndTradeBadge.tsx
+++ components/step/6/ReturnsAndTradeBadge.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import ProofOfAlphaStatement from '../../../public/assets/step/6/proof-of-alpha-statement@2x.png';
-import TwitterCard from '../../../public/assets/step/6/twitter-card-finalize@2x.png';
+import ProofOfAlphaStatement from '../../../public/assets/step/6/proof-of-alpha-statement.png';
+import TwitterCard from '../../../public/assets/step/6/twitter-card-finalize.png';
 
 const ReturnsAndTradeBadge = () => {
   return (
