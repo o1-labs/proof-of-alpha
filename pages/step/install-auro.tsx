d940d6c94007d035a8ec63c46b44b9c0c5f0ea1c
ymekuria <ymekuria@gmail.com>
2022-04-08 16:13:41 -0700

    feat: remove auro wallet modal flow on user education page

--- pages/step/install-auro.tsx
+++ pages/step/install-auro.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image';
 import type { NextPage } from 'next';
 import AuroChrome from '../../public/assets/step/auro-install/auro-install.png';
-import Header from '../../components/common/Header';
 import ProgressBar from '../../components/common/ProgressBar';
 import HeadLine from '../../components/common/HeadLine';
 import SubHeadLine from '../../components/common/SubHeadLine';
 import Layout from '../../components/common/Layout';
 import AuroBottomNavButtons from '../../components/common/AuroBottomNavButtons';
+import ZKLogo from '../../public/assets/common/zk-logo.svg';
 
 const Auro: NextPage = () => {
   return (
     <Layout backGroundColor="gradient" layoutStyle="min-container">
-      <Header />
+      <div className="relative flex items-center justify-center md:py-5 2xl:py-9 ">
+        <div className="w-18 md:w-20 2xl:w-28">
+          <Image src={ZKLogo} alt="zk logo" />
+        </div>
+      </div>
 
       <div className="flex flex-col items-center">
         <div className=" flex h-screen w-7/12 min-w-fit flex-col md:w-7/12 2xl:w-7/12  3xl:w-6/12">
