8548e0776a8269acf4e30de36048ac90bd522ffc
ymekuria <ymekuria@gmail.com>
2022-04-08 12:04:57 -0700

    fix: adjust result page statement layout on medium screens

--- components/result/StatementSection.tsx
+++ components/result/StatementSection.tsx
@@ -18,13 +18,13 @@ const StatementSection = ({ txid }) => {
         <div className=" absolute top-0 right-0 w-6/12 2xl:w-7/12 3xl:w-7/12">
           <Image src={StatementTopStamp} alt="Stamp" priority />
         </div>
-        <div className="ml-20 flex justify-between md:ml-0 ">
+        <div className="ml-18 flex justify-around md:ml-0 ">
           <div className="flex  flex-col  items-center 3xl:space-y-4 ">
             <div className="relative flex min-w-[320px] flex-col items-center justify-center 3xl:w-full">
               <ProofOfAlphaStatement />
             </div>
           </div>
-          <div className=" flex flex-col items-center space-y-7 pl-28 ">
+          <div className=" mr-18 flex flex-col items-center space-y-7 pl-28 md:mr-0">
             <p className="tracking-widest">SHARE YOUR STATEMENT</p>
             <Link href={`/statement/${txid}`} passHref>
               <ShareButton label="COPY LINK" />
