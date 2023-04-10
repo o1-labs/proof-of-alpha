6c0fc55168ff4db5a01741bc2178ac36b0ad5c7d
ymekuria <ymekuria@gmail.com>
2023-04-10 04:20:45 -0600

    feat: add get trades method

--- pages/api/binance-oracle.ts
+++ pages/api/binance-oracle.ts
@@ -4,4 +4,8 @@ import { isReady, PrivateKey, Field, Signature } from 'snarkyjs';
 export default async function handler(
   req: IncomingMessage,
   res: ServerResponse
-) {}
+) {
+  async function getTrades() {
+    return [];
+  }
+}
