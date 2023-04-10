ff55cb4f0ce379fa569d38acde77a4e68974ba4b
ymekuria <ymekuria@gmail.com>
2023-04-10 04:11:28 -0600

    feat: import snarkyjs

--- pages/api/binance-oracle.ts
+++ pages/api/binance-oracle.ts
@@ -1,1 +1,7 @@
-export default async function handler(req, res) {}
+import { IncomingMessage, ServerResponse } from 'http';
+import { isReady, PrivateKey, Field, Signature } from 'snarkyjs';
+
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse
+) {}
