97d90fb2f371d06b55a827716c8f5c5715503bcd
ymekuria <ymekuria@gmail.com>
2023-07-19 01:42:07 -0700

    feat(binance-oracle.ts): add support for fetching server time from Binance API to calculate trade history start date

--- pages/api/binance-oracle.ts
+++ pages/api/binance-oracle.ts
@@ -1,11 +1,21 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { isReady, PrivateKey, Field, Signature } from 'snarkyjs';
+import { PrivateKey, Field, Signature } from 'snarkyjs';
 import jwt from 'jsonwebtoken';
+
+const BINANCE_BASE_URL = 'https://api.binance.com';
 
 export default async function handler(
   req: IncomingMessage,
   res: ServerResponse
 ) {
+  // Timestamp in ms.
+  // TODO: calculate trade history start date from option selected in ui.
+  const ninetyDaysAgo = Date.now() - 90 * 24 * 60 * 60 * 1000;
+
+  const startTime = await fetch(`${BINANCE_BASE_URL}/api/v3/time`)
+    .then((res) => res.json())
+    .then((data) => data.serverTime);
+
   async function getTrades() {
     return [];
   }
