eb16dcad77b949c857e3a43aad8bd1c1fb2572e0
ymekuria <ymekuria@gmail.com>
2023-07-19 01:57:44 -0700

    feat(binance-oracle.ts): generate signature using secret key for Binance API authentication

--- pages/api/binance-oracle.ts
+++ pages/api/binance-oracle.ts
@@ -8,6 +8,9 @@ export default async function handler(
   req: IncomingMessage,
   res: ServerResponse
 ) {
+  // TODO: use api keys as default if users does not want to use their own.
+  const apiKey = process.env.BINANCE_API_KEY;
+  const secretKey = process.env.BINANCE_SECRET_KEY;
   // Timestamp in ms.
   // TODO: calculate trade history start date from option selected in ui.
   const ninetyDaysAgo = Date.now() - 90 * 24 * 60 * 60 * 1000;
@@ -17,6 +20,8 @@ export default async function handler(
     .then((data) => data.serverTime);
 
   const query = `symbol=ETHUSDT&startTime=${ninetyDaysAgo}&endTime=${startTime}`;
+
+  const signature = jwt.sign(query, secretKey);
 
   async function getTrades() {
     return [];
