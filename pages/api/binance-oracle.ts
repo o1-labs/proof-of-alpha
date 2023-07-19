fd002e233626b648dfec83f87071b112e9c0fb0f
ymekuria <ymekuria@gmail.com>
2023-07-19 00:59:27 -0700

    chore(package.json): add jsonwebtoken package as a dependency

--- pages/api/binance-oracle.ts
+++ pages/api/binance-oracle.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import { isReady, PrivateKey, Field, Signature } from 'snarkyjs';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(
   req: IncomingMessage,
