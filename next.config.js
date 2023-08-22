33cecfb50906e9b3c6a0b7360fbc6e04eee8e2d5
ymekuria <ymekuria@gmail.com>
2023-08-22 08:42:23 -0700

    chore(next.config.js): add configuration for Next.js image optimization
    The `images` configuration object is added to the `next.config.js` file. This configuration enables image optimization in Next.js and sets the following options:
    - `deviceSizes`: Specifies the device sizes for which optimized images will be generated.
    - `imageSizes`: Specifies the image sizes that will be generated for each device size.
    - `domains`: Specifies the domains from which images will be loaded. (Currently empty)
    - `path`: Specifies the path where optimized images will be served from.
    - `loader`: Specifies the default loader to be used for loading images.

--- next.config.js
+++ next.config.js
@@ -4,5 +4,13 @@ module.exports = {
   experimental: {
     esmExternals: false
   },
-  outputFileTracing: true
+  outputFileTracing: true,
+
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: [],
+    path: '/_next/image',
+    loader: 'default' // default loader
+  }
 };
