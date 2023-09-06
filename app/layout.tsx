f542a1121dd5bc95ddde822a7c757cbb46805d69
ymekuria <ymekuria@gmail.com>
2023-09-06 04:28:47 -0600

    feat(layout.tsx): add favicon and meta tags for better user experience and branding
    The changes in this commit add favicon and meta tags to the layout component. This improves the user experience by displaying a custom icon in the browser tab and on the home screen of mobile devices. It also adds meta tags for better SEO and branding.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicon/android-chrome-512x512.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
   );
