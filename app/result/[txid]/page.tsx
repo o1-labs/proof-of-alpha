ccfa372d68abc45ca58ecaafd73ec7fe6f2dc24a
ymekuria <ymekuria@gmail.com>
2023-09-06 02:53:24 -0600

    feat(page.tsx): migrate page component for displaying poa result to app router

--- app/result/[txid]/page.tsx
+++ app/result/[txid]/page.tsx
@@ -1,7 +1,8 @@
+'use client';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import SocialBadgeSection from '../../components/result/SocialBadgeSection';
-import StatementSection from '../../components/result/StatementSection';
+import SocialBadgeSection from '../../../components/result/SocialBadgeSection';
+import StatementSection from '../../../components/result/StatementSection';
 
 const Prove: NextPage = () => {
   const router = useRouter();
