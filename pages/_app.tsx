201f6311aab5185b29f3004702c201e6197dfc26
ymekuria <ymekuria@gmail.com>
2023-09-06 12:19:18 -0600

    refactor(_app.tsx): remove unnecessary favicon and manifest links from Head component
    The favicon and manifest links were removed from the Head component in order to simplify the code and remove unnecessary dependencies. These links were not being used in the application and were therefore removed to improve code cleanliness.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,35 +5,6 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="shortcut icon"
-          type="image/png"
-          sizes="512x512"
-          href="/favicon/android-chrome-512x512.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-
       <Component {...pageProps} />
     </>
   );
